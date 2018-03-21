3725ee26cc52cc85c61b0068f9920865119465fa
Bria Morgan <calypsomatic@gmail.com>
2018-03-21 09:34:31 -0400

    PICSURE-10 improved resourcewebclienttest a tad

--- pic-sure-resources/pic-sure-resource-api/src/test/java/edu/harvard/dbmi/avillach/ResourceWebClientTest.java
+++ pic-sure-resources/pic-sure-resource-api/src/test/java/edu/harvard/dbmi/avillach/ResourceWebClientTest.java
@@ -2,24 +2,12 @@ package edu.harvard.dbmi.avillach;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import edu.harvard.dbmi.avillach.domain.*;
-import edu.harvard.dbmi.avillach.service.HttpClientUtil;
 import edu.harvard.dbmi.avillach.service.ResourceWebClient;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 
-import javax.json.Json;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -30,22 +18,16 @@ import static org.junit.Assert.*;
 
 public class ResourceWebClientTest {
 
- //   protected static String endpointUrl;
     private final static ObjectMapper json = new ObjectMapper();
     //TODO what to do about this token
-    private final static String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0fGF2bGJvdEBkYm1pLmhtcy5oYXJ2YXJkLmVkdSIsImVtYWlsIjoiYXZsYm90QGRibWkuaG1zLmhhcnZhcmQuZWR1In0.51TYsm-uw2VtI8aGawdggbGdCSrPJvjtvzafd2Ii9NU";
-//    private final static String testURL = "http://localhost:8080/pic-sure-api-wildfly-2.0.0-SNAPSHOT/pic-sure/v1.4";
+//    private final static String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0fGF2bGJvdEBkYm1pLmhtcy5oYXJ2YXJkLmVkdSIsImVtYWlsIjoiYXZsYm90QGRibWkuaG1zLmhhcnZhcmQuZWR1In0.51TYsm-uw2VtI8aGawdggbGdCSrPJvjtvzafd2Ii9NU";
+    private final static String token = "testToken";
     private final static int port = 8079;
     private final static String testURL = "http://localhost:"+port;
     private final ResourceWebClient cut = new ResourceWebClient();
 
     @Rule
     public WireMockClassRule wireMockRule = new WireMockClassRule(port);
-
-  /*  @BeforeClass
-    public static void beforeClass() {
-        endpointUrl = System.getProperty("service.url");
-    }*/
 
     @Test
     public void testInfo() throws JsonProcessingException{
@@ -56,9 +38,9 @@ public class ResourceWebClientTest {
                 .withStatus(200)
               .withBody(resourceInfo)));
 
+        //Should throw an error if any parameters are missing
         try {
-            //Try it missing info
-            ResourceInfo result = cut.info(testURL, null);
+            cut.info(testURL, null);
             fail();
         } catch (Exception e) {
             //TODO Will change what this error actually says/does
@@ -66,18 +48,45 @@ public class ResourceWebClientTest {
         }
         Map<String, String> credentials = new HashMap<>();
         credentials.put(ResourceWebClient.BEARER_TOKEN_KEY, token);
-
         try {
-            //Try it missing info
-            ResourceInfo result = cut.info(null, credentials);
+            cut.info(null, credentials);
             fail();
         } catch (Exception e) {
             //TODO Will change what this error actually says/does
             assertEquals("Missing resource URL", e.getMessage());
         }
 
+        //Assuming everything goes right
         ResourceInfo result = cut.info(testURL, credentials);
         assertNotNull("Result should not be null", result);
+
+        //What if the resource has a problem?
+        wireMockRule.stubFor(any(urlEqualTo("/info"))
+                .willReturn(aResponse()
+                        .withStatus(500)));
+
+        try {
+            cut.info(testURL, credentials);
+            fail();
+        } catch (Exception e) {
+            //TODO Will change what this error actually says/does
+            assertTrue( e.getMessage().contains("Resource returned 500"));
+        }
+
+        //What if resource returns the wrong type of object for some reason?
+        String incorrectResponse = json.writeValueAsString(new SearchResults());
+        wireMockRule.stubFor(any(urlEqualTo("/info"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withBody(incorrectResponse)));
+
+        try {
+            cut.info(testURL, credentials);
+            fail();
+        } catch (Exception e) {
+            //TODO Will change what this error actually says/does
+            assertTrue( e.getMessage().contains("Incorrect object type returned"));
+        }
     }
 
     @Test
@@ -89,27 +98,54 @@ public class ResourceWebClientTest {
                         .withStatus(200)
                         .withBody(searchResults)));
 
+        //Should throw an error if any parameters are missing
         try {
-            //Try it missing info
-            SearchResults result = cut.search(testURL, null);
+            cut.search(testURL, null);
             fail();
         } catch (Exception e) {
             //TODO Will change what this error actually says/does
             assertEquals("Missing query request info", e.getMessage());
         }
         QueryRequest request = new QueryRequest();
-
         try {
-            //Try it missing info
-            SearchResults result = cut.search(null, request);
+            cut.search(null, request);
             fail();
         } catch (Exception e) {
             //TODO Will change what this error actually says/does
             assertEquals("Missing resource URL", e.getMessage());
         }
 
+        //If everything goes right
         SearchResults result = cut.search(testURL, request);
         assertNotNull("Result should not be null", result);
+
+        //What if the resource has a problem?
+        wireMockRule.stubFor(any(urlEqualTo("/search"))
+                .willReturn(aResponse()
+                        .withStatus(500)));
+
+        try {
+            cut.search(testURL, request);
+            fail();
+        } catch (Exception e) {
+            //TODO Will change what this error actually says/does
+            assertTrue( e.getMessage().contains("Resource returned 500"));
+        }
+
+        //What if resource returns the wrong type of object for some reason?
+        String incorrectResponse = json.writeValueAsString(new ResourceInfo());
+        wireMockRule.stubFor(any(urlEqualTo("/search"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withBody(incorrectResponse)));
+
+        try {
+            cut.search(testURL, request);
+            fail();
+        } catch (Exception e) {
+            //TODO Will change what this error actually says/does
+            assertTrue( e.getMessage().contains("Incorrect object type returned"));
+        }
     }
 
     @Test
@@ -121,41 +157,69 @@ public class ResourceWebClientTest {
                         .withStatus(200)
                         .withBody(queryResults)));
 
+        //Should fail if any parameters are missing
         try {
-            //Try it missing info
-            QueryResults result = cut.query(testURL, null);
+            cut.query(testURL, null);
             fail();
         } catch (Exception e) {
             //TODO Will change what this error actually says/does
             assertEquals("Missing query request info", e.getMessage());
         }
         QueryRequest request = new QueryRequest();
-
         try {
-            //Try it missing info
-            QueryResults result = cut.query(null, request);
+            cut.query(null, request);
             fail();
         } catch (Exception e) {
             //TODO Will change what this error actually says/does
             assertEquals("Missing resource URL", e.getMessage());
         }
 
+        //Everything goes correctly
         QueryResults result = cut.query(testURL, request);
         assertNotNull("Result should not be null", result);
+
+        //What if the resource has a problem?
+        wireMockRule.stubFor(any(urlEqualTo("/query"))
+                .willReturn(aResponse()
+                        .withStatus(500)));
+
+        try {
+            cut.query(testURL, request);
+            fail();
+        } catch (Exception e) {
+            //TODO Will change what this error actually says/does
+            assertTrue( e.getMessage().contains("Resource returned 500"));
+        }
+
+        //What if resource returns the wrong type of object for some reason?
+        String incorrectResponse = json.writeValueAsString(new ResourceInfo());
+        wireMockRule.stubFor(any(urlEqualTo("/query"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withBody(incorrectResponse)));
+
+        try {
+            cut.query(testURL, request);
+            fail();
+        } catch (Exception e) {
+            //TODO Will change what this error actually says/does
+            assertTrue( e.getMessage().contains("Incorrect object type returned"));
+        }
     }
 
     @Test
     public void testQueryResult() throws JsonProcessingException{
         String queryResults = json.writeValueAsString(new QueryResults());
+        UUID testUUID = new UUID(1, 1);
 
         wireMockRule.stubFor(any(urlMatching("/query/.*/result"))
                 .willReturn(aResponse()
                         .withStatus(200)
                         .withBody(queryResults)));
 
+        //Should fail if missing any parameters
         try {
-            //Try it missing info
-            QueryResults result = cut.queryResult(testURL, new UUID(1,1), null);
+            cut.queryResult(testURL, testUUID, null);
             fail();
         } catch (Exception e) {
             //TODO Will change what this error actually says/does
@@ -163,40 +227,67 @@ public class ResourceWebClientTest {
         }
         Map<String, String> credentials = new HashMap<>();
         credentials.put(ResourceWebClient.BEARER_TOKEN_KEY, token);
-
         try {
-            //Try it missing info
-            QueryResults result = cut.queryResult(testURL, null, credentials);
+            cut.queryResult(testURL, null, credentials);
             fail();
         } catch (Exception e) {
             //TODO Will change what this error actually says/does
             assertEquals("Missing query id", e.getMessage());
         }
         try {
-            //Try it missing info
-            QueryResults result = cut.queryResult(null, new UUID(1,1), credentials);
+            cut.queryResult(null, testUUID, credentials);
             fail();
         } catch (Exception e) {
             //TODO Will change what this error actually says/does
             assertEquals("Missing resource URL", e.getMessage());
         }
 
-        QueryResults result = cut.queryResult(testURL,new UUID(1,1), credentials);
+        //Everything should work here
+        QueryResults result = cut.queryResult(testURL,testUUID, credentials);
         assertNotNull("Result should not be null", result);
+
+        //What if the resource has a problem?
+        wireMockRule.stubFor(any(urlMatching("/query/.*/result"))
+                .willReturn(aResponse()
+                        .withStatus(500)));
+
+        try {
+            cut.queryResult(testURL, testUUID, credentials);
+            fail();
+        } catch (Exception e) {
+            //TODO Will change what this error actually says/does
+            assertTrue( e.getMessage().contains("Resource returned 500"));
+        }
+
+        //What if resource returns the wrong type of object for some reason?
+        String incorrectResponse = json.writeValueAsString(new ResourceInfo());
+        wireMockRule.stubFor(any(urlMatching("/query/.*/result"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withBody(incorrectResponse)));
+
+        try {
+            cut.queryResult(testURL, testUUID, credentials);
+            fail();
+        } catch (Exception e) {
+            //TODO Will change what this error actually says/does
+            assertTrue( e.getMessage().contains("Incorrect object type returned"));
+        }
     }
 
     @Test
     public void testQueryStatus() throws JsonProcessingException{
         String queryStatus = json.writeValueAsString(new QueryStatus());
+        UUID testUUID = new UUID(1, 1);
 
         wireMockRule.stubFor(any(urlMatching("/query/.*/status"))
                 .willReturn(aResponse()
                         .withStatus(200)
                         .withBody(queryStatus)));
 
+        //Fails with any missing parameters
         try {
-            //Try it missing info
-            QueryStatus result = cut.queryStatus(testURL, new UUID(1,1), null);
+            cut.queryStatus(testURL, testUUID, null);
             fail();
         } catch (Exception e) {
             //TODO Will change what this error actually says/does
@@ -204,25 +295,51 @@ public class ResourceWebClientTest {
         }
         Map<String, String> credentials = new HashMap<>();
         credentials.put(ResourceWebClient.BEARER_TOKEN_KEY, token);
-
         try {
-            //Try it missing info
-            QueryStatus result = cut.queryStatus(testURL, null, credentials);
+            cut.queryStatus(testURL, null, credentials);
             fail();
         } catch (Exception e) {
             //TODO Will change what this error actually says/does
             assertEquals("Missing query id", e.getMessage());
         }
         try {
-            //Try it missing info
-            QueryStatus result = cut.queryStatus(null, new UUID(1,1), credentials);
+            cut.queryStatus(null, testUUID, credentials);
             fail();
         } catch (Exception e) {
             //TODO Will change what this error actually says/does
             assertEquals("Missing resource URL", e.getMessage());
         }
 
-        QueryStatus result = cut.queryStatus(testURL,new UUID(1,1), credentials);
+        //Everything should work here
+        QueryStatus result = cut.queryStatus(testURL,testUUID, credentials);
         assertNotNull("Result should not be null", result);
+
+        //What if the resource has a problem?
+        wireMockRule.stubFor(any(urlMatching("/query/.*/status"))
+                .willReturn(aResponse()
+                        .withStatus(500)));
+
+        try {
+            cut.queryStatus(testURL, testUUID, credentials);
+            fail();
+        } catch (Exception e) {
+            //TODO Will change what this error actually says/does
+            assertTrue( e.getMessage().contains("Resource returned 500"));
+        }
+
+        //What if resource returns the wrong type of object for some reason?
+        String incorrectResponse = json.writeValueAsString(new ResourceInfo());
+        wireMockRule.stubFor(any(urlMatching("/query/.*/status"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withBody(incorrectResponse)));
+
+        try {
+            cut.queryStatus(testURL, testUUID, credentials);
+            fail();
+        } catch (Exception e) {
+            //TODO Will change what this error actually says/does
+            assertTrue( e.getMessage().contains("Incorrect object type returned"));
+        }
     }
 }
