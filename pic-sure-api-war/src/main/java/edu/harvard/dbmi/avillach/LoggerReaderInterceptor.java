ec10f0c34b0577155aba5aa31c1c0e5d9609a84a
Bria Morgan <calypsomatic@gmail.com>
2018-08-28 09:26:19 -0400

    PICSURE-148 streamlined credential redaction

--- pic-sure-api-war/src/main/java/edu/harvard/dbmi/avillach/LoggerReaderInterceptor.java
+++ pic-sure-api-war/src/main/java/edu/harvard/dbmi/avillach/LoggerReaderInterceptor.java
@@ -1,12 +1,9 @@
 package edu.harvard.dbmi.avillach;
 
-import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.commons.io.IOUtils;
 
-import javax.json.JsonObject;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.ext.Provider;
 import javax.ws.rs.ext.ReaderInterceptor;
@@ -14,9 +11,9 @@ import javax.ws.rs.ext.ReaderInterceptorContext;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Provider
 public class LoggerReaderInterceptor implements ReaderInterceptor {
@@ -28,36 +25,13 @@ public class LoggerReaderInterceptor implements ReaderInterceptor {
         InputStream inputStream = interceptorContext.getInputStream();
         String requestContent = IOUtils.toString(inputStream);
 
-        ObjectNode requestNode = (ObjectNode) new ObjectMapper().readTree(requestContent);
-        requestNode.remove("resourceCredentials");
-        Iterator<String> fields = requestNode.fieldNames();
+        JsonNode requestNode = new ObjectMapper().readTree(requestContent);
+        //Redact any credentials
+        Set<Map.Entry<String, JsonNode>> filtered = StreamSupport.stream(Spliterators.spliteratorUnknownSize(requestNode.fields(), Spliterator.NONNULL), false)
+                .filter(s -> !s.getKey().equals("resourceCredentials") && !s.getKey().contains("BEARER_TOKEN"))
+                .collect(Collectors.toSet());
 
-        List<String> fieldsToRemove = new ArrayList<>();
-        while (fields.hasNext()){
-            String key = fields.next();
-            if (key.contains("BEARER_TOKEN")){
-                fieldsToRemove.add(key);
-            }
-        }
-
-        for (String key : fieldsToRemove){
-            requestNode.remove(key);
-        }
-
-        //Redact resourceCredentials
-  /*      int begin = requestContent.indexOf("resourceCredentials");
-        int end = requestContent.indexOf("}", begin);
-        String redacted = begin == -1 ? requestContent : requestContent.substring(0, begin-1) + requestContent.substring(end+2);
-        //Redact any BEARER_TOKENs
-        begin = redacted.indexOf("BEARER_TOKEN");
-        while( begin != -1){
-            int realbegin = redacted.substring(0, begin).lastIndexOf("\"");
-            end = redacted.indexOf("}", begin);
-            redacted = realbegin == -1 ? redacted : redacted.substring(0, realbegin-1) + redacted.substring(end+1);
-            begin = redacted.indexOf("BEARER_TOKEN");
-        }*/
-
-        interceptorContext.setProperty("requestContent", requestNode);
+        interceptorContext.setProperty("requestContent", filtered);
         interceptorContext.setInputStream(new ByteArrayInputStream(requestContent.getBytes()));
 
         return interceptorContext.proceed();
