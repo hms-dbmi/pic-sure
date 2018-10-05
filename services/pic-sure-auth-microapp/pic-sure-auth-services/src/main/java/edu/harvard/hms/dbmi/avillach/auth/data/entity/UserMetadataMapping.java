77d02c3ce6ca2d8cd68ce628d44cbc2668868c61
Jason Stedman <namdets@gmail.com>
2018-10-05 14:50:58 -0400

    added mock endpoints for user metadata mapping so development can move forward

---
+++ services/pic-sure-auth-microapp/pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/data/entity/UserMetadataMapping.java
@@ -0,0 +1,43 @@
+package edu.harvard.hms.dbmi.avillach.auth.data.entity;
+
+import javax.persistence.Entity;
+
+import edu.harvard.dbmi.avillach.data.entity.BaseEntity;
+
+@Entity
+public class UserMetadataMapping extends BaseEntity {
+	
+	private String connectionId;
+	
+	private String generalMetadataJsonPath;
+	
+	private String auth0MetadataJsonPath;
+
+	public String getConnectionId() {
+		return connectionId;
+	}
+
+	public UserMetadataMapping setConnectionId(String connectionId) {
+		this.connectionId = connectionId;
+		return this;
+	}
+
+	public String getGeneralMetadataJsonPath() {
+		return generalMetadataJsonPath;
+	}
+
+	public UserMetadataMapping setGeneralMetadataJsonPath(String generalMetadataJsonPath) {
+		this.generalMetadataJsonPath = generalMetadataJsonPath;
+		return this;
+	}
+
+	public String getAuth0MetadataJsonPath() {
+		return auth0MetadataJsonPath;
+	}
+
+	public UserMetadataMapping setAuth0MetadataJsonPath(String auth0MetadataJsonPath) {
+		this.auth0MetadataJsonPath = auth0MetadataJsonPath;
+		return this;
+	}
+	
+}
---
+++ services/pic-sure-auth-microapp/pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/service/UserMetadataMappingService.java
@@ -0,0 +1,42 @@
+package edu.harvard.hms.dbmi.avillach.auth.service;
+
+import edu.harvard.hms.dbmi.avillach.auth.data.entity.UserMetadataMapping;
+import edu.harvard.hms.dbmi.avillach.auth.data.repository.UserMetadataMappingRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Service handling business logic for UserMetadataMappings.
+ */
+public class UserMetadataMappingService extends BaseEntityService<UserMetadataMapping>{
+
+    Logger logger = LoggerFactory.getLogger(UserMetadataMappingService.class);
+
+    @Inject
+    UserMetadataMappingRepository userMetadataMappingRepo;
+
+    public UserMetadataMappingService() {
+        super(UserMetadataMapping.class);
+    }
+
+    public List<UserMetadataMapping> getAllMappingsForConnection(String connectionId) {
+    		return userMetadataMappingRepo.findByConnection(connectionId);
+    }
+    
+    public List<UserMetadataMapping> getAllMappingsForConnectionMock(String connectionId){
+    		List<UserMetadataMapping> allMappings = List.of(
+    				new UserMetadataMapping().setConnectionId("ldap-connector").setGeneralMetadataJsonPath("$.email").setAuth0MetadataJsonPath("$.email"),
+    				new UserMetadataMapping().setConnectionId("nih-gov-prod").setGeneralMetadataJsonPath("$.nih-userid").setAuth0MetadataJsonPath("$.identities.userid"),
+    				new UserMetadataMapping().setConnectionId("github").setGeneralMetadataJsonPath("$.full_name").setAuth0MetadataJsonPath("$.name"),
+    				new UserMetadataMapping().setConnectionId("github").setGeneralMetadataJsonPath("$.email").setAuth0MetadataJsonPath("$.emails[?(@.primary == true].email")
+    				);
+    		return allMappings.stream().filter((UserMetadataMapping mapping)->{
+    			return mapping.getConnectionId().equalsIgnoreCase(connectionId);
+    		}).collect(Collectors.toList());
+    }
+    
+}
