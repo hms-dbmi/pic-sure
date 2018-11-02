623768fb9cbf971dba75adfbbde0ac18bfe188cd
Bria Morgan <calypsomatic@gmail.com>
2018-11-02 13:23:44 -0400

    PICSURE-206 check for constraint violations

--- services/pic-sure-auth-microapp/pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/rest/ConnectionWebService.java
+++ services/pic-sure-auth-microapp/pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/rest/ConnectionWebService.java
@@ -1,5 +1,6 @@
 package edu.harvard.hms.dbmi.avillach.auth.rest;
 
+import edu.harvard.dbmi.avillach.util.response.PICSUREResponse;
 import edu.harvard.hms.dbmi.avillach.auth.data.entity.Connection;
 import edu.harvard.hms.dbmi.avillach.auth.data.repository.ConnectionRepository;
 import edu.harvard.hms.dbmi.avillach.auth.service.BaseEntityService;
@@ -39,7 +40,7 @@ public class ConnectionWebService extends BaseEntityService<Connection> {
     @Consumes(MediaType.APPLICATION_JSON)
     @Path("/")
     public Response addConnection(List<Connection> connections){
-        return addEntity(connections, connectionRepo);
+        return addEntity(connections);
     }
 
     @PUT
@@ -54,5 +55,18 @@ public class ConnectionWebService extends BaseEntityService<Connection> {
     @Path("/{connectionId}")
     public Response removeById(@PathParam("connectionId") final String connectionId) {
         return removeEntityById(connectionId, connectionRepo);
+    }
+
+    private Response addEntity(List<Connection> connections){
+        for (Connection c : connections){
+            if (c.getSubPrefix() == null || c.getRequiredFields() == null || c.getLabel() == null || c.getId() == null){
+                return PICSUREResponse.protocolError("Id, Label, Subprefix, and RequiredFields cannot be null");
+            }
+            Connection conn = connectionRepo.findConnectionById(c.getId());
+            if (conn != null){
+                return PICSUREResponse.protocolError("Id must be unique, a connection with id " + c.getId() + " already exists in the database");
+            }
+        }
+        return addEntity(connections, connectionRepo);
     }
 }
