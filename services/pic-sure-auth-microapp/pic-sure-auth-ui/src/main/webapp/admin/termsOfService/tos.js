d7018ec02b03267d95024b30d0ac7ceb3e372e2f
Bria Morgan <calypsomatic@gmail.com>
2018-11-09 11:09:44 -0500

    PICSURE-228 prevent navigation before tos accepted

--- services/pic-sure-auth-microapp/pic-sure-auth-ui/src/main/webapp/admin/termsOfService/tos.js
+++ services/pic-sure-auth-microapp/pic-sure-auth-ui/src/main/webapp/admin/termsOfService/tos.js
@@ -10,18 +10,30 @@ define(["backbone","handlebars", "text!termsOfService/tos.hbs", "picSure/picsure
             },
             acceptTOS: function () {
                 picsureFunctions.acceptTOS(function(){
+                    this.toggleNavigationButtons(false);
                     if (sessionStorage.redirection_url) {
                         window.location = sessionStorage.redirection_url;
                     }
                     else {
                         history.pushState({}, "", "userManagement");
                     }
-                })
+                }.bind(this))
+            },
+            toggleNavigationButtons: function(disable) {
+                console.log("toggling buttons");
+                if (disable) {
+                    $("#userMgmt-header").removeAttr('href');
+                    $("#cnxn-header").removeAttr('href');
+                } else {
+                    $("#userMgmt-header").attr("href","/userManagement");
+                    $("#cnxn-header").attr("href","/connectionManagement");
+                }
             },
             render : function(){
                 picsureFunctions.getLatestTOS(function (content) {
                     this.model.set("content", content);
                     this.$el.html(this.template({content: content}));
+                    this.toggleNavigationButtons(true);
                 }.bind(this));
             }
         });
