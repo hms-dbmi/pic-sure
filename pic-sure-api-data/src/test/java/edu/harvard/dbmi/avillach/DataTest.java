c27011766e384b3107735846717c0902dc87ff04
Yu Zhang <yu_zhang@hms.harvard.edu>
2018-11-26 22:18:04 -0500

    Added unit test for this

--- pic-sure-api-data/src/test/java/edu/harvard/dbmi/avillach/DataTest.java
+++ pic-sure-api-data/src/test/java/edu/harvard/dbmi/avillach/DataTest.java
@@ -1,14 +1,26 @@
 package edu.harvard.dbmi.avillach;
 
+import edu.harvard.dbmi.avillach.data.entity.BaseEntity;
+import edu.harvard.dbmi.avillach.data.entity.User;
 import org.junit.Test;
+
+import java.util.UUID;
+
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
  */
 public class DataTest {
+
 	@Test
-	//TODO:
-	public void dummyTest() {
-		
+	public void BaseEntityBasicFunctionsTest() {
+		BaseEntity user = new User();
+		user.setUuid(UUID.fromString("6ef9387a-4cde-4253-bd47-0bdc74ff76ab"));
+
+		BaseEntity user2 = new User();
+		user2.setUuid(UUID.fromString("6ef9387a-4cde-4253-bd47-0bdc74ff76ab"));
+
+		assertEquals(user, user2);
 	}
 }
