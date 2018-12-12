73f2a0895023f222f58aaada8bf7eda9b39a6874
Yu Zhang <yu_zhang@hms.harvard.edu>
2018-12-12 12:13:20 -0500

    picsure 235 access changed email has properly sent out

--- services/pic-sure-auth-microapp/pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/service/UpdateUserListener.java
+++ services/pic-sure-auth-microapp/pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/service/UpdateUserListener.java
@@ -3,12 +3,17 @@ package edu.harvard.hms.dbmi.avillach.auth.service;
 import edu.harvard.hms.dbmi.avillach.auth.data.entity.User;
 import org.hibernate.envers.boot.internal.EnversService;
 import org.hibernate.envers.event.spi.BaseEnversCollectionEventListener;
-import org.hibernate.event.spi.*;
+import org.hibernate.event.spi.PostCollectionUpdateEvent;
+import org.hibernate.event.spi.PostCollectionUpdateEventListener;
+import org.hibernate.event.spi.PostInsertEvent;
+import org.hibernate.event.spi.PostInsertEventListener;
 import org.hibernate.persister.entity.EntityPersister;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class UpdateUserListener extends BaseEnversCollectionEventListener implements PostInsertEventListener, PostCollectionUpdateEventListener {
+public class UpdateUserListener extends BaseEnversCollectionEventListener implements PostInsertEventListener,
+        PostCollectionUpdateEventListener{
+
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     //Cannot inject into stateless eventlistener
