7c424e4cf8751eb53c9d6eae7b4e8d9bc31b68bd
Gabor Korodi <gabor_korodi@hms.harvard.edu>
2019-01-30 17:14:17 -0500

    Fix typo

--- services/pic-sure-auth-microapp/pic-sure-auth-ui/src/main/webapp/main.js
+++ services/pic-sure-auth-microapp/pic-sure-auth-ui/src/main/webapp/main.js
@@ -1,6 +1,6 @@
 require.config({
 	baseUrl: "/",
-    urlArgs: "version=1.0.0",
+	urlArgs: "version=1.0.0",
 	paths: {
 		jquery: 'webjars/jquery/3.3.1/jquery.min',
 		underscore: 'webjars/underscorejs/1.8.3/underscore-min',
@@ -8,11 +8,11 @@ require.config({
 		bootstrap: 'webjars/bootstrap/3.3.7-1/js/bootstrap.min',
 		backbone: 'webjars/backbonejs/1.3.3/backbone-min',
 		text: 'webjars/requirejs-text/2.0.15/text',
-		'auth0-js': "/webjars/auth0.js/9.2.3/build/auth0",
-        Noty: 'webjars/noty/3.1.4/lib/noty',
-        userManagement: "userManagement/",
-        common: "common/",
-        tos: "tos/"
+		'auth0-js': "webjars/auth0.js/9.2.3/build/auth0",
+		Noty: 'webjars/noty/3.1.4/lib/noty',
+		userManagement: "userManagement/",
+		common: "common/",
+		tos: "tos/"
     },
     shim: {
         "bootstrap": {
