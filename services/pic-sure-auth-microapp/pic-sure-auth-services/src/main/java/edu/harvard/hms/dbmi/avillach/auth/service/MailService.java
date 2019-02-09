2f648282d58306696806c332ed830f5d4b2ff591
Jason Stedman <namdets@gmail.com>
2019-02-08 19:57:58 -0500

    handling when user emails come from general metadata

--- services/pic-sure-auth-microapp/pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/service/MailService.java
+++ services/pic-sure-auth-microapp/pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/service/MailService.java
@@ -1,10 +1,13 @@
 package edu.harvard.hms.dbmi.avillach.auth.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.mustachejava.DefaultMustacheFactory;
 import com.github.mustachejava.Mustache;
 import com.github.mustachejava.MustacheFactory;
 import edu.harvard.hms.dbmi.avillach.auth.JAXRSConfiguration;
 import edu.harvard.hms.dbmi.avillach.auth.data.entity.User;
+
+import org.hibernate.mapping.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,6 +18,9 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.io.StringWriter;
 import java.io.Writer;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MailService {
     private Logger logger = LoggerFactory.getLogger(MailService.class);
@@ -22,13 +28,22 @@ public class MailService {
     Mustache accessEmail = mf.compile("accessEmail.mustache");
 
     //TODO Where/how to store this for real?
-    private String systemName = System.getenv("systemName");
+    private String systemName = System.getenv("java:global/systemName");
 
     public void sendUsersAccessEmail(User user){
         try {
             Message message = new MimeMessage(JAXRSConfiguration.mailSession);
-            if (user.getEmail() != null){
-                message.setRecipient(Message.RecipientType.TO, new InternetAddress(user.getEmail()));
+            String email = user.getEmail();
+            if(email == null) {
+            		HashMap<String,String> metadata = new HashMap<String, String>((java.util.Map<? extends String, ? extends String>) new ObjectMapper().readValue(user.getGeneralMetadata(), Map.class));
+            		List<String> emailKeys = metadata.keySet().stream().filter((key)->{return key.toLowerCase().contains("email");}).collect(Collectors.toList());
+            		if(emailKeys.size()>0) {
+            			email = metadata.get(emailKeys.get(0));
+            			user.setEmail(email);
+            		}
+            }
+			if (email != null){
+                message.setRecipient(Message.RecipientType.TO, new InternetAddress(email));
                 //TODO Is the subject configurable as well?  What should it say?
                 message.setSubject("Your Access To " + systemName);
                 String body = generateBody(user);
