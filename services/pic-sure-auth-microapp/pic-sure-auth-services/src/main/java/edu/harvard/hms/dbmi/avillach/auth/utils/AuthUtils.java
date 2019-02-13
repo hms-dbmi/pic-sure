4a4ed7e052cc722dd0a2020c2c581d25cbce41c5
Jason Stedman <namdets@gmail.com>
2019-02-13 13:20:02 -0500

    putting back in handling for non-base64 encoded tokens

--- services/pic-sure-auth-microapp/pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/utils/AuthUtils.java
+++ services/pic-sure-auth-microapp/pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/utils/AuthUtils.java
@@ -11,43 +11,51 @@ import java.io.UnsupportedEncodingException;
 
 public class AuthUtils {
 
-    private static Logger logger = LoggerFactory.getLogger(AuthUtils.class);
+	private static Logger logger = LoggerFactory.getLogger(AuthUtils.class);
 
-    /**
-     * support both Base64 encrypted and non-Base64 encrypted
-     * @param clientSecret
-     * @param token
-     * @return
-     */
-    public static Jws<Claims> parseToken(String clientSecret, String token)
-            throws NotAuthorizedException{
-        Jws<Claims> jws;
+	/**
+	 * support both Base64 encrypted and non-Base64 encrypted
+	 * @param clientSecret
+	 * @param token
+	 * @return
+	 */
+	public static Jws<Claims> parseToken(String clientSecret, String token)
+			throws NotAuthorizedException{
+		Jws<Claims> jws;
 
-        try {
-            jws = Jwts.parser().setSigningKey(clientSecret.getBytes()).parseClaimsJws(token);
-        } catch (SignatureException e) {
-            try {
-                jws = Jwts.parser().setSigningKey(Base64.decodeBase64(clientSecret
-                        .getBytes("UTF-8")))
-                        .parseClaimsJws(token);
-            } catch (UnsupportedEncodingException ex){
-                logger.error("parseToken() clientSecret encoding UTF-8 is not supported. "
-                        + ex.getClass().getSimpleName() + ": " + ex.getMessage());
-                throw new NotAuthorizedException("encoding is not supported");
-            } catch (JwtException | IllegalArgumentException ex) {
-                logger.error("parseToken() throws: " + e.getClass().getSimpleName() + ", " + e.getMessage());
-                throw new NotAuthorizedException(ex.getClass().getSimpleName());
-            }
-        } catch (JwtException | IllegalArgumentException e) {
-            logger.error("parseToken() throws: " + e.getClass().getSimpleName() + ", " + e.getMessage());
-            throw new NotAuthorizedException(e.getClass().getSimpleName());
-        }
+		try {
+			jws = Jwts.parser().setSigningKey(clientSecret.getBytes()).parseClaimsJws(token);
+		} catch (SignatureException e) {
+			try {
+				try {
+					// handle if client secret is base64 encoded
+					jws = Jwts.parser().setSigningKey(Base64.decodeBase64(clientSecret
+							.getBytes("UTF-8")))
+							.parseClaimsJws(token);
+				} catch(SignatureException ex) {
+					// handle if client secret is not base64 encoded
+					jws = Jwts.parser().setSigningKey(clientSecret
+							.getBytes("UTF-8"))
+							.parseClaimsJws(token);
+				} 
+			} catch (UnsupportedEncodingException ex){
+				logger.error("parseToken() clientSecret encoding UTF-8 is not supported. "
+						+ ex.getClass().getSimpleName() + ": " + ex.getMessage());
+				throw new NotAuthorizedException("encoding is not supported");
+			} catch (JwtException | IllegalArgumentException ex) {
+				logger.error("parseToken() throws: " + e.getClass().getSimpleName() + ", " + e.getMessage());
+				throw new NotAuthorizedException(ex.getClass().getSimpleName());
+			}
+		} catch (JwtException | IllegalArgumentException e) {
+			logger.error("parseToken() throws: " + e.getClass().getSimpleName() + ", " + e.getMessage());
+			throw new NotAuthorizedException(e.getClass().getSimpleName());
+		}
 
-        if (jws == null) {
-            logger.error("parseToken() get null for jws body by parsing Token - " + token);
-            throw new NotAuthorizedException("please contact admin to see the log");
-        }
+		if (jws == null) {
+			logger.error("parseToken() get null for jws body by parsing Token - " + token);
+			throw new NotAuthorizedException("please contact admin to see the log");
+		}
 
-        return jws;
-    }
+		return jws;
+	}
 }
