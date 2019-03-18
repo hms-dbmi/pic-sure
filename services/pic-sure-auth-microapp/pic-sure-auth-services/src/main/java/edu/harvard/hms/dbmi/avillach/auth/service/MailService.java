0e962e3718efa2893779a8514bfd679dc39ad580
Alex Nikitin <aleksandr.nikitin@childrens.harvard.edu>
2019-03-18 11:38:38 -0400

    #PICSURE-262 rework for send email to admin about user "Denied access to system"

--- services/pic-sure-auth-microapp/pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/service/MailService.java
+++ services/pic-sure-auth-microapp/pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/service/MailService.java
@@ -1,5 +1,7 @@
 package edu.harvard.hms.dbmi.avillach.auth.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.mustachejava.DefaultMustacheFactory;
 import com.github.mustachejava.Mustache;
 import com.github.mustachejava.MustacheFactory;
@@ -20,53 +22,67 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.StringWriter;
-import java.io.Writer;
+import java.util.Map;
 
 public class MailService {
 	private static Logger logger = LoggerFactory.getLogger(MailService.class);
 	private static MustacheFactory mf = new DefaultMustacheFactory();
 
-	private static Mustache accessEmail = null;
-	
-	private void loadTemplates() {
-		try(FileReader reader = new FileReader(JAXRSConfiguration.userActivationTemplatePath);){
-			accessEmail = mf.compile(reader, "accessEmail");
+	private Mustache loadTemplates(String templateFile) throws IOException {
+		try(FileReader reader = new FileReader(JAXRSConfiguration.emailTemplatePath + templateFile)){
+			return mf.compile(reader, templateFile);
 		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			logger.error(e.getMessage(), e);
+			throw e;
 		} catch (IOException e) {
-			e.printStackTrace();
+			logger.error(e.getMessage(), e);
+			throw e;
 		}
 	}
 
 	public void sendUsersAccessEmail(User user){
-		if(accessEmail==null) {
-			loadTemplates();
-		}
-		try {
-			Message message = new MimeMessage(JAXRSConfiguration.mailSession);
-			String email = user.getEmail();
-			if (email != null){
-				InternetAddress fromEmail = new InternetAddress(JAXRSConfiguration.userActivationReplyTo);
-				message.setFrom(fromEmail);
-				message.setReplyTo(new Address[] {fromEmail});
-				message.setRecipient(Message.RecipientType.TO, new InternetAddress(email));
-				//TODO Is the subject configurable as well?  What should it say?
-				message.setSubject("Your Access To " + JAXRSConfiguration.systemName);
-				String body = generateBody(user);
-				message.setText(body);
-				Transport.send(message);
-			} else {
-				logger.error("User " + user.getSubject() + " has no email");
-			}
-		} catch (MessagingException e){
-			logger.error(e.getMessage(), e);
-		} catch (Exception e){
-			logger.error(e.getMessage(), e);
+		if (user.getEmail() == null) {
+			logger.error("User " + user.getSubject() + " has no email");
+		} else {
+			sendEmail("accessEmail.mustache", user.getEmail(),"Your Access To " + JAXRSConfiguration.systemName, new AccessEmail(user));
 		}
 	}
 
-	public String generateBody(User u) {
-		Writer writer = accessEmail.execute(new StringWriter(),new AccessEmail(u));
-		return writer.toString();
+	public void sendDeniedAccessEmail(JsonNode userInfo){
+		logger.info("Sending 'Access Denied' email to " + JAXRSConfiguration.adminUsers + ". User: " + userInfo.get("user_id").asText());
+		ObjectMapper mapper = new ObjectMapper();
+		Map<String, Object> scope = mapper.convertValue(userInfo, Map.class);
+		scope.put("systemName", JAXRSConfiguration.systemName);
+		if (userInfo == null) {
+			logger.error("User " + scope.get("user_id") + " has no email");
+			return;
+		}
+		sendEmail("deniedAccessEmail.mustache", JAXRSConfiguration.adminUsers, "User denied access to " + JAXRSConfiguration.systemName, scope);
+	}
+
+	/**
+	 * Generate email from template and send it.
+	 * @param template Name of the template.
+	 * @param to Recipients
+	 * @param subject Subject of the email
+	 * @param scope Object that contains attributes for template. e.g.: Map
+	 * @throws MessagingException
+	 */
+	private void sendEmail(String template, String to, String subject, Object scope) {
+		logger.debug("sendEmail(String, String, String, Object) - start");
+		try {
+			Mustache email = loadTemplates(template);
+			Message message = new MimeMessage(JAXRSConfiguration.mailSession);
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+			//TODO Is the subject configurable per email?  What should it say?
+			message.setSubject(subject);
+			message.setText(email.execute(new StringWriter(), scope).toString());
+			Transport.send(message);
+		} catch (MessagingException me) {
+			logger.error("Failed to send email: '" + subject + "'", me);
+		}
+		catch (Exception e) {
+			logger.error("Error occurred while trying to send email '" + subject + "'", e);
+		}
 	}
 }
