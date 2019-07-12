2cf3d9b19361abed21ddde170d71b4e5f560c217
Jason Stedman <namdets@gmail.com>
2019-07-12 07:20:04 -0400

    adding comments

--- services/pic-sure-hpds/processing/src/main/java/edu/harvard/hms/dbmi/avillach/hpds/processing/CountProcessor.java
+++ services/pic-sure-hpds/processing/src/main/java/edu/harvard/hms/dbmi/avillach/hpds/processing/CountProcessor.java
@@ -2,10 +2,7 @@ package edu.harvard.hms.dbmi.avillach.hpds.processing;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Set;
-import java.util.TreeSet;
 
 import edu.harvard.hms.dbmi.avillach.hpds.data.query.Query;
 import edu.harvard.hms.dbmi.avillach.hpds.exception.NotEnoughMemoryException;
@@ -17,10 +14,25 @@ public class CountProcessor extends AbstractProcessor {
 		super();
 	}
 
+	/**
+	 * Retrieves a list of patient ids that are valid for the query result and returns the size of that list.
+	 * 
+	 * @param query
+	 * @return
+	 * @throws TooManyVariantsException
+	 */
 	public int runCounts(Query query) throws TooManyVariantsException {
 		return getPatientSubsetForQuery(query).size();
 	}
 
+	/**
+	 * Returns a separate count for each field in query.crossCountFields when that field is added
+	 * as a requiredFields entry for the base query.
+	 * 
+	 * @param query
+	 * @return
+	 * @throws TooManyVariantsException
+	 */
 	public HashMap<String, Integer> runCrossCounts(Query query) throws TooManyVariantsException {
 		HashMap<String, Integer> counts = new HashMap<>();
 		TooManyVariantsException[] exceptions = new TooManyVariantsException[1];
@@ -39,6 +51,10 @@ public class CountProcessor extends AbstractProcessor {
 		return counts;
 	}
 
+	/**
+	 * Until we have a count based query that takes longer than 30 seconds to run, we should discourage
+	 * running them asynchronously in the backend as this results in unnecessary request-response cycles.
+	 */
 	@Override
 	public void runQuery(Query query, AsyncResult asyncResult) throws NotEnoughMemoryException {
 		throw new UnsupportedOperationException("Counts do not run asynchronously.");
