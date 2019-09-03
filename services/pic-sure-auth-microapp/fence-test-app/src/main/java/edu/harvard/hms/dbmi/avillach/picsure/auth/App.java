56b76e42c5af16297f1440327b63349448f6c5c3
Jason Stedman <namdets@gmail.com>
2019-09-03 12:59:16 -0400

    initial fence integration POC

---
+++ services/pic-sure-auth-microapp/fence-test-app/src/main/java/edu/harvard/hms/dbmi/avillach/picsure/auth/App.java
@@ -0,0 +1,13 @@
+package edu.harvard.hms.dbmi.avillach.picsure.auth;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
