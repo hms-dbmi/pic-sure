bce3adb4ba59309e55d68015711ae684ad2c6248
Yu Zhang <yu_zhang@hms.harvard.edu>
2019-09-24 11:10:35 -0400

    package-info added

---
+++ services/pic-sure-auth-microapp/pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/security/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contains security filters that check every incoming request to PSAMA
+ */
+package edu.harvard.hms.dbmi.avillach.auth.security;
---
+++ services/pic-sure-auth-microapp/pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/utils/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contains useful utility classes
+ */
+package edu.harvard.hms.dbmi.avillach.auth.utils;
