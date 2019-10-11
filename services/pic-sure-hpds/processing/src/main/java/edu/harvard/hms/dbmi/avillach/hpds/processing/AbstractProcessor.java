7b1acc0922d321cba16ce58a75d83ee264f4cb47
Jason Stedman <namdets@gmail.com>
2019-10-11 09:50:38 -0400

    reducing console spam

--- services/pic-sure-hpds/processing/src/main/java/edu/harvard/hms/dbmi/avillach/hpds/processing/AbstractProcessor.java
+++ services/pic-sure-hpds/processing/src/main/java/edu/harvard/hms/dbmi/avillach/hpds/processing/AbstractProcessor.java
@@ -283,7 +283,7 @@ public abstract class AbstractProcessor {
 			}
 			// TODO : This is probably not necessary, see TODO below. 
 			String bitmaskString = bitmask.toString(2);
-			System.out.println("or'd masks : " + bitmaskString);
+			//System.out.println("or'd masks : " + bitmaskString);
 			PhenoCube<String> idCube;
 			try {
 				idCube = ID_CUBE_NAME.contentEquals("NONE") ? null : (PhenoCube<String>) store.get(ID_CUBE_NAME);
@@ -505,7 +505,7 @@ public abstract class AbstractProcessor {
 							}
 							Set<Integer> ids = new TreeSet<Integer>();
 							String bitmaskString = matchingPatients.toString(2);
-							log.info("or'd masks : " + bitmaskString);
+							//log.info("or'd masks : " + bitmaskString);
 							PhenoCube<String> patientIdCube = ID_CUBE_NAME.contentEquals("NONE")? null : (PhenoCube<String>) store.get(ID_CUBE_NAME);
 							for(int x = 2;x < bitmaskString.length()-2;x++) {
 								if('1'==bitmaskString.charAt(x)) {
