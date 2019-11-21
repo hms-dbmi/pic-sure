13de7d4f2704a49fe72d9f2c2da8456bd3912bcb
Nathan Chu <chu.nathan@gmail.com>
2019-11-21 11:13:04 -0500

    Compress query field when writing and decompress when reading

--- pic-sure-api-data/src/main/java/edu/harvard/dbmi/avillach/data/entity/Query.java
+++ pic-sure-api-data/src/main/java/edu/harvard/dbmi/avillach/data/entity/Query.java
@@ -1,9 +1,21 @@
 package edu.harvard.dbmi.avillach.data.entity;
 
-import edu.harvard.dbmi.avillach.util.PicSureStatus;
-
-import javax.persistence.*;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.Date;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+
+import edu.harvard.dbmi.avillach.util.PicSureStatus;
 
 @Entity(name = "query")
 public class Query extends BaseEntity {
@@ -19,8 +31,9 @@ public class Query extends BaseEntity {
 	private String resourceResultId;
 
 	//Original query request
-	@Column(length = 8192)
-	private String query;
+	@Lob
+    @Column(columnDefinition="BLOB")
+	private byte[] query;
 
 	@ManyToOne
 	@JoinColumn(name = "resourceId")
@@ -70,11 +83,42 @@ public class Query extends BaseEntity {
 	}
 
 	public String getQuery() {
-		return query;
+		
+		if (this.query == null || this.query.length == 0) {
+            return "";
+        }
+		
+		String outStr = "";
+		try {
+	        GZIPInputStream gis = new GZIPInputStream(new ByteArrayInputStream(this.query));
+	        BufferedReader bf = new BufferedReader(new InputStreamReader(gis, "UTF-8"));
+	        String line;
+	        while ((line=bf.readLine())!=null) {
+	          outStr += line;
+	        }
+	        System.out.println("Output String lenght : " + outStr.length());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        return outStr;
 	}
 
-	public void setQuery(String query) {
-		this.query = query;
+	public void setQuery(String queryStr) {
+		if (queryStr == null || queryStr.length() == 0) {
+			this.query = new byte[0];
+            return;
+        }
+       
+		try {
+			ByteArrayOutputStream obj=new ByteArrayOutputStream();
+			GZIPOutputStream gzip = new GZIPOutputStream(obj);
+			gzip.write(queryStr.getBytes("UTF-8"));
+	        gzip.close();
+	        this.query = obj.toByteArray();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        
 	}
 
 	public byte[] getMetadata() {
