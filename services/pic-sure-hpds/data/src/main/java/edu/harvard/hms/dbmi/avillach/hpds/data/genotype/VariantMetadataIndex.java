290ae8ffa16f68964431574b9b0a97451e1881e9
anilk2hms <anilkumar.degala@childrens.harvard.edu>
2020-03-16 10:27:05 -0400

    ALS-112 - HPDS should index INFO column records by VariantSpec

--- services/pic-sure-hpds/data/src/main/java/edu/harvard/hms/dbmi/avillach/hpds/data/genotype/VariantMetadataIndex.java
+++ services/pic-sure-hpds/data/src/main/java/edu/harvard/hms/dbmi/avillach/hpds/data/genotype/VariantMetadataIndex.java
@@ -1,9 +1,74 @@
 package edu.harvard.hms.dbmi.avillach.hpds.data.genotype;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+import edu.harvard.hms.dbmi.avillach.hpds.storage.FileBackedByteIndexedStorage;
+
 /**
  * To be implemented as part of ALS-112
  * 
  */
-public class VariantMetadataIndex {
+public class VariantMetadataIndex implements Serializable {
+
+	private static final long serialVersionUID = 5917054606643971537L;
+
+	public static String storageFile = "/opt/local/hpds/all/VariantMetadataStorage.bin";
+	public static String binFile = "/opt/local/hpds/all/VariantMetadata.javabin";
+	FileBackedByteIndexedStorage<String, String[]> fbbiStorage;
+	FileBackedByteIndexedStorage<String, String[]> fbbiStorageBin;  
+	
+	public VariantMetadataIndex() { 
+		//Moving default construction of resources to separate initialize methods (loading index and reading index data)
+	}
+
+	public void initializeWrites() throws FileNotFoundException, IOException, ClassNotFoundException {
+		fbbiStorage = new FileBackedByteIndexedStorage<>(String.class, String[].class, new File(storageFile));
+	}
+	
+	public void initializeRead() throws FileNotFoundException, IOException, ClassNotFoundException {
+		ObjectInputStream in = new ObjectInputStream(new GZIPInputStream(new FileInputStream(new File(binFile))));
+		fbbiStorageBin = (FileBackedByteIndexedStorage<String, String[]>) in.readObject();
+	}
+
+	public String[] findBySingleVariantSpec(String string) throws Exception {
+		return fbbiStorageBin.get(string);
+	}
+
+	public Map<String, String[]> findByMultipleVariantSpec(List<String> varientSpecList) throws Exception {
+		Map<String, String[]> result = varientSpecList.stream().collect(HashMap::new, (m, v) -> {
+			try {
+				m.put(v, this.findBySingleVariantSpec(v));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}, HashMap::putAll);
+		return result;
+	}
+
+	public void put(String specNotation, String[] array) throws IOException {
+		fbbiStorage.put(specNotation, array);
+	}
+
+	public void complete() {
+		fbbiStorage.complete();
+	}
+
+	public void writeObject() throws FileNotFoundException, IOException, ClassNotFoundException {
+		ObjectOutputStream out = new ObjectOutputStream(new GZIPOutputStream(new FileOutputStream(new File(binFile))));
+		out.writeObject(fbbiStorage);
+		out.close();
+	}
 
 }
--- services/pic-sure-hpds/etl/src/main/java/edu/harvard/hms/dbmi/avillach/hpds/etl/genotype/VariantMetadataLoader.java
+++ services/pic-sure-hpds/etl/src/main/java/edu/harvard/hms/dbmi/avillach/hpds/etl/genotype/VariantMetadataLoader.java
@@ -1,5 +1,28 @@
 package edu.harvard.hms.dbmi.avillach.hpds.etl.genotype;
 
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.zip.GZIPOutputStream;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import edu.harvard.hms.dbmi.avillach.hpds.data.genotype.VariantMetadataIndex;
+import edu.harvard.hms.dbmi.avillach.hpds.data.genotype.VariantSpec;
+
 /**
  * 
  * To be implemented as part of ALS-112
@@ -7,9 +30,51 @@ package edu.harvard.hms.dbmi.avillach.hpds.etl.genotype;
  */
 public class VariantMetadataLoader {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	private static final int  
+	CHROMOSOME_COLUMN = 0,
+	POS_COLUMN = 1,
+	REF_ALLELES_COLUMN = 3,
+	ALT_ALLELES_COLUMN = 4,
+	INFO_COLUMN = 7;
 	
+	 
+	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException { 
+		String vcfFile = "/opt/local/hpds/vcfInput/chr14.1kg.phase3_small.20130502.vcf"; 
+		VariantMetadataIndex vmi = new VariantMetadataIndex();  
+		vmi.initializeWrites();
+		
+		CSVParser parser = null;
+		Reader reader = null;
+		try {
+			reader = Files.newBufferedReader(Paths.get(vcfFile));
+			parser = new CSVParser(reader, CSVFormat.DEFAULT.withDelimiter('\t').withSkipHeaderRecord(false)); 
+			Iterator<CSVRecord> iterator = parser.iterator();  
+			
+			boolean isRowData = false;  
+			while(iterator.hasNext()) { 
+			    CSVRecord csvRecord = iterator.next(); 
+			    if(csvRecord.get(0).startsWith("#CHROM")) {  
+			    	csvRecord = iterator.next();
+			    	isRowData = true;
+			    } 
+			    
+			    if(isRowData) {  
+			    	String key = csvRecord.get(CHROMOSOME_COLUMN).trim() +  "," + csvRecord.get(POS_COLUMN).trim() +  "," + csvRecord.get(REF_ALLELES_COLUMN).trim() +  "," + csvRecord.get(ALT_ALLELES_COLUMN).trim();
+			    	String value  = csvRecord.get(INFO_COLUMN).trim(); 
+			    	VariantSpec variantSpec = new VariantSpec(key); 
+			    	
+			    	//TODO:: Handle multiple info values 
+			    	List<String> valueList = new ArrayList<String>();
+			    	valueList.add(value); 
+			    	vmi.put(variantSpec.specNotation(), valueList.stream().toArray(size -> new String[size])); 
+			    } 
+			} 
+			vmi.complete(); 
+			vmi.writeObject(); 
+		} catch (Exception e) {
+			parser.close();
+			e.printStackTrace();
+		}
+
+	}   
 }
