8e7bf9bd8efb9bacdbb2bf524f9c1450b61e8589
anilk2hms <anilkumar.degala@childrens.harvard.edu>
2020-03-17 13:42:04 -0400

    ALS-122 - refactor and code review changes

--- services/pic-sure-hpds/etl/src/main/java/edu/harvard/hms/dbmi/avillach/hpds/etl/genotype/VariantMetadataLoader.java
+++ services/pic-sure-hpds/etl/src/main/java/edu/harvard/hms/dbmi/avillach/hpds/etl/genotype/VariantMetadataLoader.java
@@ -7,10 +7,17 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.io.Reader;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.TreeMap;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.stream.Collectors;
 import java.util.zip.GZIPOutputStream;
 
 import org.apache.commons.csv.CSVFormat;
@@ -28,10 +35,46 @@ import edu.harvard.hms.dbmi.avillach.hpds.data.genotype.VariantSpec;
 public class VariantMetadataLoader {
 
 	private static final int 
-	INFO_COLUMN = 7;  
-	 
-	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException { 
+	INFO_COLUMN = 7,  
+	FILE_COLUMN = 0,
+	CHROMOSOME_COLUMN = 1,
+	ANNOTATED_FLAG_COLUMN = 2,
+	GZIP_FLAG_COLUMN=3,
+	SAMPLE_IDS_COLUMN=4,
+	PATIENT_IDS_COLUMN=5,
+	//These columns are to support a future feature, ignore them for now.
+	SAMPLE_RELATIONSHIPS_COLUMN=6,
+	RELATED_SAMPLE_IDS_COLUMN=7;	
+	
+	public static void main(String[] args) throws Exception{ 
+		/*
+		File vcfIndexFile = new File("/opt/local/hpds/vcfIndex.tsv");
+		List<File> vcfFiles = new ArrayList<>();
+ 		CSVParser parser;
+		try {
+			parser = CSVParser.parse(vcfIndexFile, Charset.forName("UTF-8"), CSVFormat.DEFAULT.withDelimiter('\t').withSkipHeaderRecord(true)); 
+			final boolean[] horribleHeaderSkipFlag = {false}; 
+			parser.forEach((CSVRecord r)->{
+				if(horribleHeaderSkipFlag[0]) {
+					File vcfFileLocal = new File(r.get(FILE_COLUMN)); 
+					vcfFiles.add(vcfFileLocal);
+				}else {
+					horribleHeaderSkipFlag[0] = true;
+				}
+			});
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		vcfFiles.stream().forEach(System.out::println);
+		
+		*/
+		
 		String vcfFile = "/opt/local/hpds/vcfInput/chr14.1kg.phase3_small.20130502.vcf"; 
+		processFile(vcfFile);
+	}
+	 
+	public static void processFile(String vcfFile) throws Exception {  
 		VariantMetadataIndex vmi = new VariantMetadataIndex(); 
 		
 		CSVParser parser = null;
@@ -56,7 +99,7 @@ public class VariantMetadataLoader {
 			vmi.complete(); 
 			
 			ObjectOutputStream out = new ObjectOutputStream(new GZIPOutputStream(new FileOutputStream(new File(VariantMetadataIndex.binFile))));
-			out.writeObject(vmi.getFbbis());
+			out.writeObject(vmi);
 			out.close();
 			
 			
