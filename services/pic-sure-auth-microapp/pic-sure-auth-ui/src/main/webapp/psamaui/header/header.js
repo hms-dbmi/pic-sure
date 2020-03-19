d516fee07403a68b3e3411961d030df7b824f1f2
Jason Stedman <namdets@gmail.com>
2020-03-19 13:58:24 -0400

    adding in logoPath

--- services/pic-sure-auth-microapp/pic-sure-auth-ui/src/main/webapp/psamaui/header/header.js
+++ services/pic-sure-auth-microapp/pic-sure-auth-ui/src/main/webapp/psamaui/header/header.js
@@ -1,5 +1,5 @@
-define(["backbone","handlebars", "text!header/header.hbs", "common/session", "picSure/userFunctions","picSure/applicationFunctions", "text!options/modal.hbs","text!header/userProfile.hbs", "util/notification"],
-		function(BB, HBS, template, session, userFunctions, applicationFunctions,modalTemplate, userProfileTemplate, notification){
+define(["backbone","handlebars", 'picSure/settings', "text!header/header.hbs", "common/session", "picSure/userFunctions","picSure/applicationFunctions", "text!options/modal.hbs","text!header/userProfile.hbs", "util/notification"],
+		function(BB, HBS, settings, template, session, userFunctions, applicationFunctions,modalTemplate, userProfileTemplate, notification){
 	var headerView = BB.View.extend({
         initialize: function () {
             HBS.registerHelper('not_contains', function (array, object, opts) {
@@ -118,6 +118,7 @@ define(["backbone","handlebars", "text!header/header.hbs", "common/session", "pi
                 if (window.location.pathname == "/psamaui/userProfile"){
                     applicationFunctions.fetchApplications(this, function(applications){
                         this.$el.html(this.template({
+                        	logoPath: settings.logoPath,
                             privileges: [],
                             applications: applications
                                 .filter(function (app) {
