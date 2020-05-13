f834b66a8a07ece7f2848bf00c48a9a71114fa68
Nathan Chu <chu.nathan@gmail.com>
2020-05-13 12:54:44 -0400

    update jquery version

--- services/pic-sure-auth-microapp/pic-sure-auth-ui/src/main/webapp/psamaui/main.js
+++ services/pic-sure-auth-microapp/pic-sure-auth-ui/src/main/webapp/psamaui/main.js
@@ -2,7 +2,7 @@ require.config({
 	baseUrl: "/psamaui/",
     urlArgs: "version=1.0.0",
 	paths: {
-		jquery: 'webjars/jquery/3.3.1/jquery.min',
+		jquery: 'webjars/jquery/3.5.1/jquery.min',
 		underscore: 'webjars/underscorejs/1.8.3/underscore-min',
 		handlebars: 'webjars/handlebars/4.0.5/handlebars.min',
 		bootstrap: 'webjars/bootstrap/3.3.7-1/js/bootstrap.min',
