000d1bd06934f6cddada94faffe77635ce340ba1
chunacatsunflower <chu.nathan@gmail.com>
2020-09-17 12:33:08 -0400

    add missing import

--- services/pic-sure-hpds/service/src/main/java/edu/harvard/hms/dbmi/avillach/hpds/service/PicSureService.java
+++ services/pic-sure-hpds/service/src/main/java/edu/harvard/hms/dbmi/avillach/hpds/service/PicSureService.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.TreeMap;
