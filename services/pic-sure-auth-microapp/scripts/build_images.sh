1c4db41aaf07597e784dfb4e5ad11491cc9ccd17
Ryan Amari <ramari16@gmail.com>
2021-01-19 15:34:01 -0500

    ALS-1636: Move pic-sure-auth-ui from pic-sure-auth-microapp repo

--- services/pic-sure-auth-microapp/scripts/build_images.sh
+++ services/pic-sure-auth-microapp/scripts/build_images.sh
@@ -5,13 +5,11 @@ logger() {
 	echo "${TIMESTAMP} ${MSG}"
 }
 #
-# This script will build psama and psamaui images to be pushed to the Docker hub, from the local depository.
+# This script will build psama image to be pushed to the Docker hub, from the local depository.
 # This script will NOT push the generated images automatically.
 # This script will tag the generated images as:
 #    "dbmi/pic-sure-auth-services:${GITHUB_BRANCH}_${GIT_COMMIT_HASH}"
-# for the PSAMA back-end image and
-#    "dbmi/pic-sure-auth-ui:${GITHUB_BRANCH}_${GIT_COMMIT_HASH}"
-# for the PSAMA UI (front-end) image
+# for the PSAMA back-end image
 #
 # Currently, the script requires Java 11 (11.0.2-open) and Maven to be
 # installed on the machine that is executing the build process.
@@ -62,20 +60,6 @@ else
 fi
 cd ..
 
-# Build the PSAMA UI front-end docker image.
-#    sub-task 1., Get the current GitHub branch and commit hash
-cd pic-sure-auth-ui
-docker build . --rm --tag "dbmi/pic-sure-auth-ui:${GITHUB_BRANCH}.${GITHUB_COMMIT_HASH}"
-CMD_STATUS=$?
-if [ $CMD_STATUS -eq 0 ];
-then
-	logger "Successfully built PSAMA UI docker image dbmi/pic-sure-auth-ui:${GITHUB_BRANCH}.${GITHUB_COMMIT_HASH} locally."
-else
-	logger "Failed to build PSAMA UI docker image locally."
-	exit 255
-fi
-
 echo "Images:"
-echo "dbmi/pic-sure-auth-ui:${GITHUB_BRANCH}.${GITHUB_COMMIT_HASH}"
 echo "dbmi/pic-sure-auth-services:${GITHUB_BRANCH}.${GITHUB_COMMIT_HASH}"
 logger "Done."
