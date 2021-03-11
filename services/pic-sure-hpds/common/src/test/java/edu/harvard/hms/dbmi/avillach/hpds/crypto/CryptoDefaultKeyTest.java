c3b17ef16e580560d186775d41f75256c0ab3317
Jason Stedman <namdets@gmail.com>
2021-03-11 10:25:36 -0500

    combining test classes

--- services/pic-sure-hpds/common/src/test/java/edu/harvard/hms/dbmi/avillach/hpds/crypto/CryptoDefaultKeyTest.java
+++ services/pic-sure-hpds/common/src/test/java/edu/harvard/hms/dbmi/avillach/hpds/crypto/CryptoDefaultKeyTest.java
@@ -19,6 +19,18 @@ public class CryptoDefaultKeyTest {
 	}
 	
 	@Test
+	public void testCryptoLoadsKey() throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
+		Field field = Crypto.class.getDeclaredField("DEFAULT_ENCRYPTION_KEY_PATH");
+		field.setAccessible(true);
+		Field modifiersField = Field.class.getDeclaredField("modifiers");
+        modifiersField.setAccessible(true);
+        modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+		field.set(Crypto.class, new File("src/test/resources/test_encryption_key").getAbsolutePath());
+        Crypto.loadDefaultKey();
+        assertTrue(Crypto.hasKey(Crypto.DEFAULT_KEY_NAME));
+	}
+
+	@Test
 	public void testEncryptDecrypt() throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
 		Field field = Crypto.class.getDeclaredField("DEFAULT_ENCRYPTION_KEY_PATH");
 		field.setAccessible(true);
