233df3e705bc95416f4f4c8b2df1ec67360dab7f
GeorgeC <gcolon021@gmail.com>
2023-04-12 16:40:33 -0400

    Migrated PicsureRS.java to swagger v3
    Updated all annotations in PicsureRS.java to use swagger v3 annotations.

--- pic-sure-api-war/src/main/java/edu/harvard/dbmi/avillach/PicsureRS.java
+++ pic-sure-api-war/src/main/java/edu/harvard/dbmi/avillach/PicsureRS.java
@@ -11,16 +11,12 @@ import edu.harvard.dbmi.avillach.domain.*;
 import edu.harvard.dbmi.avillach.service.PicsureInfoService;
 import edu.harvard.dbmi.avillach.service.PicsureQueryService;
 import edu.harvard.dbmi.avillach.service.PicsureSearchService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 
 @Path("/")
 @Produces("application/json")
 @Consumes("application/json")
-@Api
 public class PicsureRS {
 	
 	@Inject
@@ -34,8 +30,6 @@ public class PicsureRS {
 
 	@POST
 	@Path("/info/{resourceId}")
-//	@ApiOperation(value = "Returns information about the provided resource")
-//	@ApiResponse(code = 200, message = "Resource information", response = ResourceInfo.class)
 	@Operation(summary = "Returns information about the provided resource",
 			description = "Returns information about the provided resource",
 			tags = { "info" },
@@ -50,8 +44,8 @@ public class PicsureRS {
 					)
 			)}
 	)
-	public ResourceInfo resourceInfo(@ApiParam(value="The UUID of the resource to fetch information about") @PathParam("resourceId") String resourceId,
-									 @ApiParam(value="Object with field named 'resourceCredentials' which is a key-value map, " +
+	public ResourceInfo resourceInfo(@Parameter(description = "The UUID of the resource to fetch information about") @PathParam("resourceId") String resourceId,
+									 @Parameter(description="Object with field named 'resourceCredentials' which is a key-value map, " +
 											 "key is identifier for resource, value is token for resource") QueryRequest credentialsQueryRequest) {
 		System.out.println("Resource info requested for : " + resourceId);
 		return infoService.info(UUID.fromString(resourceId), credentialsQueryRequest);
@@ -59,64 +53,152 @@ public class PicsureRS {
 	
 	@GET
 	@Path("/info/resources")
-	@ApiOperation(value = "Returns list of resources available")
-	@ApiResponse(code = 200, message = "Resource information", response = Map.class, responseContainer = "Map<UUID,String>")
+	@Operation(
+			description = "Returns list of resources available",
+			responses = {
+					@io.swagger.v3.oas.annotations.responses.ApiResponse(
+							responseCode = "200",
+							description = "Resource information",
+							content = @io.swagger.v3.oas.annotations.media.Content(
+									schema = @io.swagger.v3.oas.annotations.media.Schema(
+											implementation = Map.class
+									)
+							)
+					)
+			}
+	)
 	public Map<UUID,String> resources(){
 		return infoService.resources();
 	}
 	
 	@POST
 	@Path("/search/{resourceId}")
-	@ApiOperation(value = "Searches for paths on the given resource matching the supplied search term")
-	@ApiResponse(code = 200, message = "Search results", response = SearchResults.class)
-	public SearchResults search(@ApiParam(value="The UUID of the resource to search") @PathParam("resourceId") UUID resourceId,
-								@ApiParam(value="Object containing credentials map under 'resourceCredentials' " +
+	@Operation(
+			description = "Searches for paths on the given resource matching the supplied search term",
+			responses = {
+					@io.swagger.v3.oas.annotations.responses.ApiResponse(
+							responseCode = "200",
+							description = "Search results",
+							content = @io.swagger.v3.oas.annotations.media.Content(
+									schema = @io.swagger.v3.oas.annotations.media.Schema(
+											implementation = SearchResults.class
+									)
+							)
+					)
+			}
+	)
+	public SearchResults search(@Parameter(description="The UUID of the resource to search") @PathParam("resourceId") UUID resourceId,
+								@Parameter(description="Object containing credentials map under 'resourceCredentials' " +
 										"and search term under 'query'") QueryRequest searchQueryRequest) {
 		return searchService.search(resourceId, searchQueryRequest);
 	}
 	
 	@POST
 	@Path("/query")
-	@ApiOperation(value = "Submits a query to the given resource")
-	@ApiResponse(code = 200, message = "Query status", response = QueryStatus.class)
-	public QueryStatus query(@ApiParam(value="Object containing credentials map under 'resourceCredentials' " +
+	@Operation(
+			description = "Submits a query to the given resource",
+			responses = {
+					@io.swagger.v3.oas.annotations.responses.ApiResponse(
+							responseCode = "200",
+							description = "Query status",
+							content = @io.swagger.v3.oas.annotations.media.Content(
+									schema = @io.swagger.v3.oas.annotations.media.Schema(
+											implementation = QueryStatus.class
+									)
+							)
+					)
+			}
+	)
+	public QueryStatus query(@Parameter(description="Object containing credentials map under 'resourceCredentials' " +
 									 "and query object under 'query'")QueryRequest dataQueryRequest) {
 		return queryService.query(dataQueryRequest);
 	}
 	
 	@POST
 	@Path("/query/{queryId}/status")
-	@ApiOperation(value = "Returns the status of the given query")
-	@ApiResponse(code = 200, message = "Query status", response = QueryStatus.class)
-	public QueryStatus queryStatus(@ApiParam(value="The UUID of the query to fetch the status of") @PathParam("queryId") UUID queryId,
-								   @ApiParam(value="Object with field named 'resourceCredentials' which is a key-value map, " +
+	@Operation(
+			description = "Returns the status of the given query",
+			responses = {
+					@io.swagger.v3.oas.annotations.responses.ApiResponse(
+							responseCode = "200",
+							description = "Query status",
+							content = @io.swagger.v3.oas.annotations.media.Content(
+									schema = @io.swagger.v3.oas.annotations.media.Schema(
+											implementation = QueryStatus.class
+									)
+							)
+					)
+			}
+	)
+	public QueryStatus queryStatus(@Parameter(description="The UUID of the query to fetch the status of") @PathParam("queryId") UUID queryId,
+								   @Parameter(description="Object with field named 'resourceCredentials' which is a key-value map, " +
 										   "key is identifier for resource, value is token for resource") QueryRequest credentialsQueryRequest) {
 		return queryService.queryStatus(queryId, credentialsQueryRequest);
 	}
 	
 	@POST
 	@Path("/query/{queryId}/result")
-	@ApiOperation(value = "Returns result for given query")
-	@ApiResponse(code = 200, message = "Query result", response = Response.class)
-	public Response queryResult(@ApiParam(value="The UUID of the query to fetch the results of") @PathParam("queryId") UUID queryId,
-								@ApiParam(value="Object with field named 'resourceCredentials' which is a key-value map, " +
+	@Operation(
+			description = "Returns result for given query",
+			responses = {
+					@io.swagger.v3.oas.annotations.responses.ApiResponse(
+							responseCode = "200",
+							description = "Query result",
+							content = @io.swagger.v3.oas.annotations.media.Content(
+									schema = @io.swagger.v3.oas.annotations.media.Schema(
+											implementation = Response.class
+									)
+							)
+					)
+			}
+	)
+	public Response queryResult(@Parameter(description = "The UUID of the query to fetch the results of") @PathParam("queryId") UUID queryId,
+								@Parameter(description = "Object with field named 'resourceCredentials' which is a key-value map, " +
 										"key is identifier for resource, value is token for resource") QueryRequest credentialsQueryRequest) {
 		return queryService.queryResult(queryId, credentialsQueryRequest);
 	}
 
 	@POST
 	@Path("/query/sync")
-	@ApiOperation(value = "Returns result for given query")
-	@ApiResponse(code = 200, message = "Query result", response = Response.class)
-	public Response querySync(@ApiParam(value="Object with field named 'resourceCredentials' which is a key-value map, " +
+//	@ApiOperation(value = "Returns result for given query")
+//	@ApiResponse(code = 200, message = "Query result", response = Response.class)
+	@Operation(
+			description = "Returns result for given query",
+			responses = {
+					@io.swagger.v3.oas.annotations.responses.ApiResponse(
+							responseCode = "200",
+							description = "Query result",
+							content = @io.swagger.v3.oas.annotations.media.Content(
+									schema = @io.swagger.v3.oas.annotations.media.Schema(
+											implementation = Response.class
+									)
+							)
+					)
+			}
+	)
+	public Response querySync(@Parameter(description="Object with field named 'resourceCredentials' which is a key-value map, " +
 										"key is identifier for resource, value is token for resource") QueryRequest credentialsQueryRequest) {
 		return queryService.querySync(credentialsQueryRequest);
 	}
 	
 	@GET
 	@Path("/query/{queryId}/metadata")
-	@ApiOperation(value = "Returns metadata for given query")
-	@ApiResponse(code = 200, message = "Query metadata", response = QueryStatus.class)
+//	@ApiOperation(value = "Returns metadata for given query")
+//	@ApiResponse(code = 200, message = "Query metadata", response = QueryStatus.class)
+	@Operation(
+			description = "Returns metadata for given query",
+			responses = {
+					@io.swagger.v3.oas.annotations.responses.ApiResponse(
+							responseCode = "200",
+							description = "Query metadata",
+							content = @io.swagger.v3.oas.annotations.media.Content(
+									schema = @io.swagger.v3.oas.annotations.media.Schema(
+											implementation = QueryStatus.class
+									)
+							)
+					)
+			}
+	)
 	public QueryStatus queryMetadata(@PathParam("queryId") UUID queryId){
 		return queryService.queryMetadata(queryId);
 	}
