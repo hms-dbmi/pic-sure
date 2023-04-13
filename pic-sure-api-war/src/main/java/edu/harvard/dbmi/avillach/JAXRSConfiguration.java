fbd9be7ec1237085033cf19c40de80697344e44a
GeorgeC <gcolon021@gmail.com>
2023-04-13 13:23:24 -0400

    Add test configuration for swagger

--- pic-sure-api-war/src/main/java/edu/harvard/dbmi/avillach/JAXRSConfiguration.java
+++ pic-sure-api-war/src/main/java/edu/harvard/dbmi/avillach/JAXRSConfiguration.java
@@ -1,5 +1,10 @@
 package edu.harvard.dbmi.avillach;
 
+import io.swagger.v3.jaxrs2.integration.JaxrsOpenApiContextBuilder;
+import io.swagger.v3.oas.integration.OpenApiConfigurationException;
+import io.swagger.v3.oas.integration.SwaggerConfiguration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,12 +13,18 @@ import javax.ejb.Startup;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import javax.servlet.ServletConfig;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Startup
 @ApplicationPath("PICSURE")
 public class JAXRSConfiguration extends Application {
+
+    @javax.ws.rs.core.Context
+     ServletConfig servletConfig;
 
     private Logger logger = LoggerFactory.getLogger(JAXRSConfiguration.class);
 
@@ -26,7 +37,28 @@ public class JAXRSConfiguration extends Application {
         logger.info("Initializing roles claim.");
         initializeRolesClaim();
         logger.info("Finished initializing roles claim.");
+        initializeSwagger();
+    }
 
+    private void initializeSwagger(){
+        OpenAPI oas = new OpenAPI();
+        Info info = new Info()
+                .title("PICSURE API")
+                .description("PICSURE API");
+        oas.info(info);
+        SwaggerConfiguration oasConfig = new SwaggerConfiguration()
+                .openAPI(oas)
+                .prettyPrint(true)
+                .resourcePackages(Stream.of("edu.harvard.dbmi.avillach").collect(Collectors.toSet()));
+        try {
+            new JaxrsOpenApiContextBuilder<>()
+                    .servletConfig(this.servletConfig)
+                    .application(this)
+                    .openApiConfiguration(oasConfig)
+                    .buildContext(true);
+        } catch (OpenApiConfigurationException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private void initializeRolesClaim(){
