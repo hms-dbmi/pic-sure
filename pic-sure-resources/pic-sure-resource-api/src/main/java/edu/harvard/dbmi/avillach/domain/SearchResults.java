2d65cbe30325ab25c52d19f9fe96179fec7147a0
GeorgeC <gcolon021@gmail.com>
2023-04-24 14:05:18 -0400

    Migrate Swagger to Nhanes
    Add all annotations and dependencies to branch to implement swagger (OpenAPI)

--- pic-sure-resources/pic-sure-resource-api/src/main/java/edu/harvard/dbmi/avillach/domain/SearchResults.java
+++ pic-sure-resources/pic-sure-resource-api/src/main/java/edu/harvard/dbmi/avillach/domain/SearchResults.java
@@ -1,8 +1,14 @@
 package edu.harvard.dbmi.avillach.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "A search results object")
 public class SearchResults {
+
+	@Schema(description = "The results of the search.")
 	Object results;
-	
+
+	@Schema(description = "The query that was used to generate the results.")
 	String searchQuery;
 
 	public Object getResults() {
