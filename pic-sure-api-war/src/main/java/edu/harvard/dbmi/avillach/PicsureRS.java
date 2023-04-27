0623c8a3f50ed37dc5195c835c1f3029ab1ab093
GeorgeC <gcolon021@gmail.com>
2023-04-27 13:25:59 -0400

    Update PicsureRS to add default value
    When sends a request there must be a default value so the optional doesn't throw a NPE.

--- pic-sure-api-war/src/main/java/edu/harvard/dbmi/avillach/PicsureRS.java
+++ pic-sure-api-war/src/main/java/edu/harvard/dbmi/avillach/PicsureRS.java
@@ -37,7 +37,8 @@ public class PicsureRS {
 	public ResourceInfo resourceInfo(@ApiParam(value="The UUID of the resource to fetch information about") @PathParam("resourceId") String resourceId,
 									 @ApiParam(value="Object with field named 'resourceCredentials' which is a key-value map, " +
 											 "key is identifier for resource, value is token for resource") QueryRequest credentialsQueryRequest,
-									 @ApiParam(value = "Optional: resource UUID of Open or Authorized Access. Utilized for logging purposes.") Optional<String> resourceUUID) {
+									 @ApiParam(value = "Optional: resource UUID of Open or Authorized Access. Utilized for logging purposes.")
+										 @QueryParam("resourceUUID") @DefaultValue("") Optional<String> resourceUUID) {
 		System.out.println("Resource info requested for : " + resourceId);
 		return infoService.info(UUID.fromString(resourceId), credentialsQueryRequest, resourceUUID);
 	}
@@ -46,7 +47,8 @@ public class PicsureRS {
 	@Path("/info/resources")
 	@ApiOperation(value = "Returns list of resources available")
 	public Map<UUID,String> resources(
-			@ApiParam(value = "Optional: resource UUID of Open or Authorized Access. Utilized for logging purposes.") Optional<String> resourceUUID) {
+			@ApiParam(value = "Optional: resource UUID of Open or Authorized Access. Utilized for logging purposes.")
+				@QueryParam("resourceUUID") @DefaultValue("") Optional<String> resourceUUID) {
 		return infoService.resources(resourceUUID);
 	}
 	
@@ -56,7 +58,8 @@ public class PicsureRS {
 	public SearchResults search(@ApiParam(value="The UUID of the resource to search") @PathParam("resourceId") UUID resourceId,
 								@ApiParam(value="Object containing credentials map under 'resourceCredentials' " +
 										"and search term under 'query'") QueryRequest searchQueryRequest,
-								@ApiParam(value = "Optional: resource UUID of Open or Authorized Access. Utilized for logging purposes.") Optional<String> resourceUUID) {
+								@ApiParam(value = "Optional: resource UUID of Open or Authorized Access. Utilized for logging purposes.")
+									@QueryParam("resourceUUID") @DefaultValue("") Optional<String> resourceUUID) {
 		return searchService.search(resourceId, searchQueryRequest, resourceUUID);
 	}
 	
@@ -64,8 +67,9 @@ public class PicsureRS {
 	@Path("/query")
 	@ApiOperation(value = "Submits a query to the given resource")
 	public QueryStatus query(@ApiParam(value="Object containing credentials map under 'resourceCredentials' " +
-									 "and query object under 'query'")QueryRequest dataQueryRequest,
-							 @ApiParam(value = "Optional: resource UUID of Open or Authorized Access. Utilized for logging purposes.") Optional<String> resourceUUID) {
+									 "and query object under 'query'") QueryRequest dataQueryRequest,
+							 @ApiParam(value = "Optional: resource UUID of Open or Authorized Access. Utilized for logging purposes.")
+							 	@QueryParam("resourceUUID") @DefaultValue("") Optional<String> resourceUUID) {
 		return queryService.query(dataQueryRequest, resourceUUID);
 	}
 	
@@ -75,7 +79,8 @@ public class PicsureRS {
 	public QueryStatus queryStatus(@ApiParam(value="The UUID of the query to fetch the status of") @PathParam("queryId") UUID queryId,
 								   @ApiParam(value="Object with field named 'resourceCredentials' which is a key-value map, " +
 										   "key is identifier for resource, value is token for resource") QueryRequest credentialsQueryRequest,
-								   @ApiParam(value = "Optional: resource UUID of Open or Authorized Access. Utilized for logging purposes.") Optional<String> resourceUUID) {
+								   @ApiParam(value = "Optional: resource UUID of Open or Authorized Access. Utilized for logging purposes.")
+									   Optional<String> resourceUUID) {
 		return queryService.queryStatus(queryId, credentialsQueryRequest, resourceUUID);
 	}
 	
@@ -85,7 +90,9 @@ public class PicsureRS {
 	public Response queryResult(@ApiParam(value="The UUID of the query to fetch the results of") @PathParam("queryId") UUID queryId,
 								@ApiParam(value="Object with field named 'resourceCredentials' which is a key-value map, " +
 										"key is identifier for resource, value is token for resource") QueryRequest credentialsQueryRequest,
-								@ApiParam(value = "Optional: resource UUID of Open or Authorized Access. Utilized for logging purposes.") Optional<String> resourceUUID) {
+								@ApiParam(value = "Optional: resource UUID of Open or Authorized Access. Utilized for logging purposes.")
+									@QueryParam("resourceUUID") @DefaultValue("")
+									Optional<String> resourceUUID) {
 		return queryService.queryResult(queryId, credentialsQueryRequest, resourceUUID);
 	}
 
@@ -94,14 +101,16 @@ public class PicsureRS {
 	@ApiOperation(value = "Returns result for given query")
 	public Response querySync(@ApiParam(value="Object with field named 'resourceCredentials' which is a key-value map, " +
 										"key is identifier for resource, value is token for resource") QueryRequest credentialsQueryRequest,
-							  @ApiParam(value = "Optional: resource UUID of Open or Authorized Access. Utilized for logging purposes.") Optional<String> resourceUUID) {
+							  @ApiParam(value = "Optional: resource UUID of Open or Authorized Access. Utilized for logging purposes.")
+							  	@QueryParam("resourceUUID") @DefaultValue("") Optional<String> resourceUUID) {
 		return queryService.querySync(credentialsQueryRequest, resourceUUID);
 	}
 	
 	@GET
 	@Path("/query/{queryId}/metadata")
 	public QueryStatus queryMetadata(@PathParam("queryId") UUID queryId,
-									 @ApiParam(value = "Optional: resource UUID of Open or Authorized Access. Utilized for logging purposes.") Optional<String> resourceUUID) {
+									 @ApiParam(value = "Optional: resource UUID of Open or Authorized Access. Utilized for logging purposes.")
+									 	@QueryParam("resourceUUID") @DefaultValue("") Optional<String> resourceUUID) {
 		return queryService.queryMetadata(queryId, resourceUUID);
 	}
 	
