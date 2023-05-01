39684fd3113f02d3c92b9360cfc9f0daba7c4290
GeorgeC <gcolon021@gmail.com>
2023-05-01 15:36:07 -0400

    Update query object example

--- pic-sure-resources/pic-sure-resource-api/src/main/java/edu/harvard/dbmi/avillach/domain/QueryRequest.java
+++ pic-sure-resources/pic-sure-resource-api/src/main/java/edu/harvard/dbmi/avillach/domain/QueryRequest.java
@@ -16,37 +16,34 @@ public class QueryRequest {
 	@Schema(description = "A string or object that contains a search term or query. Potential Expect Result Type: " +
 			"\"COUNT\", \"CROSS_COUNT\", \"INFO_COLUMN_LISTING\", \"OBSERVATION_COUNT\", \"OBSERVATION_CROSS_COUNT\"",
 			example = "{\n" +
-					"   \"resourceUUID\":\"<Resource UUID>\",\n" +
-					"   \"query\":{\n" +
-					"      \"categoryFilters\":{\n" +
-					"         \"\\\\demographics\\\\SEX\\\\\":[\n" +
-					"            \"female\"\n" +
-					"         ]\n" +
-					"      },\n" +
-					"      \"numericFilters\":{\n" +
-					"         \"\\\\demographics\\\\AGE\\\\\":{\n" +
-					"            \"min\":\"30\",\n" +
-					"            \"max\":\"50\"\n" +
-					"         }\n" +
-					"      },\n" +
-					"      \"requiredFields\":[\n" +
-					"         \n" +
-					"      ],\n" +
-					"      \"anyRecordOf\":[\n" +
-					"         \n" +
-					"      ],\n" +
-					"      \"variantInfoFilters\":[\n" +
-					"         {\n" +
-					"            \"categoryVariantInfoFilters\":{\n" +
-					"               \n" +
-					"            },\n" +
-					"            \"numericVariantInfoFilters\":{\n" +
-					"               \n" +
+					"    \"resourceUUID\": \"de273216-3b46-4dec-a330-1aa0634624ae\",\n" +
+					"    \"query\": {\n" +
+					"        \"categoryFilters\": {\n" +
+					"            \"\\\\ACT Demographics\\\\Sex\\\\\": [\n" +
+					"                \"Female\",\n" +
+					"                \"Male\"\n" +
+					"            ]\n" +
+					"        },\n" +
+					"        \"numericFilters\": {\n" +
+					"            \"\\\\ACT Demographics\\\\Age\\\\\": {\n" +
+					"                \"min\": \"5\",\n" +
+					"                \"max\": \"12\"\n" +
 					"            }\n" +
-					"         }\n" +
-					"      ],\n" +
-					"      \"expectedResultType\":\"COUNT\"\n" +
-					"   }\n" +
+					"        },\n" +
+					"        \"requiredFields\": [],\n" +
+					"        \"anyRecordOf\": [],\n" +
+					"        \"variantInfoFilters\": [\n" +
+					"            {\n" +
+					"                \"categoryVariantInfoFilters\": {\n" +
+					"                    \"Gene_with_variant\": [\n" +
+					"                        \"CHD8\"\n" +
+					"                    ]\n" +
+					"                },\n" +
+					"                \"numericVariantInfoFilters\": {}\n" +
+					"            }\n" +
+					"        ],\n" +
+					"        \"expectedResultType\": \"COUNT\"\n" +
+					"    }\n" +
 					"}")
 	private Object query;
 
