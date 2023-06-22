26695be6f4169daef0c139a584e037ce113a4385
gcolon021 <gcolon021@gmail.com>
2023-06-22 13:11:40 -0400

    Add Logging for debugging

--- pic-sure-resources/pic-sure-visualization-resource/src/main/java/edu/harvard/hms/dbmi/avillach/resource/visualization/filter/HeaderFilter.java
+++ pic-sure-resources/pic-sure-visualization-resource/src/main/java/edu/harvard/hms/dbmi/avillach/resource/visualization/filter/HeaderFilter.java
@@ -1,13 +1,14 @@
 package edu.harvard.hms.dbmi.avillach.resource.visualization.filter;
 
 import edu.harvard.hms.dbmi.avillach.resource.visualization.bean.RequestScopedHeader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.Provider;
-import java.io.IOException;
 
 @Provider
 public class HeaderFilter implements ContainerRequestFilter {
@@ -15,8 +16,11 @@ public class HeaderFilter implements ContainerRequestFilter {
     @Inject
     private RequestScopedHeader headers;
 
+    private final Logger logger = LoggerFactory.getLogger(HeaderFilter.class);
+
     @Override
-    public void filter(ContainerRequestContext containerRequestContext) throws IOException {
+    public void filter(ContainerRequestContext containerRequestContext) {
+        logger.info("HeaderFilter called with headers: " + containerRequestContext.getHeaders().toString());
         MultivaluedMap<String, String> httpHeaders = containerRequestContext.getHeaders();
         headers.setHeaders(httpHeaders);
     }
