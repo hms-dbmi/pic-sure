589644d093cf45bb09aaff9ea3554e2e0c848cee
gcolon021 <gcolon021@gmail.com>
2023-06-22 16:09:19 -0400

    Use getClasses to register the HeaderFilter.class

--- pic-sure-resources/pic-sure-visualization-resource/src/main/java/edu/harvard/hms/dbmi/avillach/resource/visualization/JAXRSConfiguration.java
+++ pic-sure-resources/pic-sure-visualization-resource/src/main/java/edu/harvard/hms/dbmi/avillach/resource/visualization/JAXRSConfiguration.java
@@ -1,5 +1,7 @@
 package edu.harvard.hms.dbmi.avillach.resource.visualization;
 
+import edu.harvard.hms.dbmi.avillach.resource.visualization.filter.HeaderFilter;
+
 import javax.inject.Inject;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
@@ -7,6 +9,8 @@ import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 @ApplicationPath("pic-sure")
 @WebListener
@@ -25,6 +29,13 @@ public class JAXRSConfiguration extends Application implements ServletContextLis
 	@Override
 	public void contextDestroyed(ServletContextEvent event) {
 		// NOOP.
+	}
+
+	@Override
+	public Set<Class<?>> getClasses() {
+		final Set<Class<?>> classes = new HashSet<>();
+		classes.add(HeaderFilter.class);
+		return classes;
 	}
 
 }
