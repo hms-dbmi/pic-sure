be495803899001599ac2e8b7e5ff0bdbbdf9670d
gcolon021 <gcolon021@gmail.com>
2023-06-23 10:58:24 -0400

    Revert JAXRSConfiguration

--- pic-sure-resources/pic-sure-visualization-resource/src/main/java/edu/harvard/hms/dbmi/avillach/resource/visualization/JAXRSConfiguration.java
+++ pic-sure-resources/pic-sure-visualization-resource/src/main/java/edu/harvard/hms/dbmi/avillach/resource/visualization/JAXRSConfiguration.java
@@ -1,7 +1,5 @@
 package edu.harvard.hms.dbmi.avillach.resource.visualization;
 
-import edu.harvard.hms.dbmi.avillach.resource.visualization.filter.HeaderFilter;
-
 import javax.inject.Inject;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
@@ -9,8 +7,6 @@ import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-import java.util.HashSet;
-import java.util.Set;
 
 @ApplicationPath("pic-sure")
 @WebListener
@@ -29,13 +25,6 @@ public class JAXRSConfiguration extends Application implements ServletContextLis
 	@Override
 	public void contextDestroyed(ServletContextEvent event) {
 		// NOOP.
-	}
-
-	@Override
-	public Set<Class<?>> getClasses() {
-		final Set<Class<?>> classes = new HashSet<>();
-		classes.add(HeaderFilter.class);
-		return classes;
 	}
 
 }
