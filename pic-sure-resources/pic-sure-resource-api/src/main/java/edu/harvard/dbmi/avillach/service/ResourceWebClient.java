18b4ba3c50508c1c25b96f3111a0e720feb1baca
Ryan Amari <ramari16@gmail.com>
2023-07-27 14:05:24 -0400

    ALS-4777: Fix path issues

--- pic-sure-resources/pic-sure-resource-api/src/main/java/edu/harvard/dbmi/avillach/service/ResourceWebClient.java
+++ pic-sure-resources/pic-sure-resource-api/src/main/java/edu/harvard/dbmi/avillach/service/ResourceWebClient.java
@@ -71,14 +71,18 @@ public class ResourceWebClient {
 
     public PaginatedSearchResult<?> searchConceptValues(String rsURL, QueryRequest queryRequest, String conceptPath, String query, Integer page, Integer size) {
         try {
-            logger.debug("Calling /info/values at ResourceURL: {}");
-            String pathName = "/search/values/";
+            logger.debug("Calling /search/values at ResourceURL: {}");
             URIBuilder uriBuilder = new URIBuilder(rsURL);
-            uriBuilder.setPath(pathName);
-            uriBuilder.addParameter("conceptPath", conceptPath);
+            String pathName = "search/values/";
+            uriBuilder.setPath(uriBuilder.getPath() + pathName);
+            uriBuilder.addParameter("genomicConceptPath", conceptPath);
             uriBuilder.addParameter("query", query);
-            uriBuilder.addParameter("page", page.toString());
-            uriBuilder.addParameter("size", size.toString());
+            if (page != null) {
+                uriBuilder.addParameter("page", page.toString());
+            }
+            if (size != null) {
+                uriBuilder.addParameter("size", size.toString());
+            }
             HttpResponse resourcesResponse = retrieveGetResponse(uriBuilder.build().toString(), createHeaders(queryRequest.getResourceCredentials()));
             if (resourcesResponse.getStatusLine().getStatusCode() != 200) {
                 logger.error("ResourceRS did not return a 200");
