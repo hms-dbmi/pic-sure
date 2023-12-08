00def617cbe6487c6a0623267190ad7ad9ba3539
Luke Sikina <lucas.skina@gmail.com>
2023-12-08 10:28:37 -0500

    mv

---
+++ services/pic-sure-services/info-service/src/test/java/edu/harvard/dbmi/avillach/infoservice/InfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.harvard.dbmi.avillach.infoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
