e7b50d5d786d706c0f0b3dfc65bdaf96baae8b9d
Ryan Amari <ramari16@gmail.com>
2024-02-16 10:58:22 -0500

    ALS-5905: Disable test that requires public genomic data

--- services/pic-sure-hpds/genomic-processor/src/test/java/edu/harvard/hms/dbmi/avillach/hpds/genomic/HpdsGenomicProcessorApplicationTests.java
+++ services/pic-sure-hpds/genomic-processor/src/test/java/edu/harvard/hms/dbmi/avillach/hpds/genomic/HpdsGenomicProcessorApplicationTests.java
@@ -1,8 +1,10 @@
 package edu.harvard.hms.dbmi.avillach.hpds.genomic;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class HpdsGenomicProcessorApplicationTests {
 
