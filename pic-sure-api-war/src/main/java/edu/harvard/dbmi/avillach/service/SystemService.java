89a823deda38075db7fd7f4a76a0d17d72650470
Gcolon021 <34667267+Gcolon021@users.noreply.github.com>
2024-04-02 16:30:14 -0400

    Release (#190)
    * [ALS-5827] Enable search by CA id
    
    - If a uuid does not match a query, instead search for common area UUID
    - If we have a collision, I'm buying a lottery ticket
    
    * [ALS-5787] Open Access StatViz is not showing "Other" (#180)
    
    * Update key length handling in limitKeySize
    
    The limitKeySize method in the VisualizationUtil class has been updated to ensure key uniqueness when keys exceed a certain length. A more robust method of shortening the keys has been implemented: if the key is longer than 45 characters, it is cut off and replaced with "..." and additional characters are appended to ensure uniqueness if needed. This helps limit key size while preserving uniqueness.
    
    * Add VisualizationUtilTests and refine key shortening logic
    
    Added a new test class, VisualizationUtilTests to validate the functionality of the VisualizationUtil class. Adjusted the 'limitKeySize' method to ensure key uniqueness when keys are shortened to a maximum length of 45 characters. If shortened keys are not unique, additional trailing characters are included until uniqueness is achieved.
    
    * Add check on length before substring
    
    * Update pic-sure-util/src/main/java/edu/harvard/dbmi/avillach/util/VisualizationUtil.java
    
    * Update pic-sure-util/src/main/java/edu/harvard/dbmi/avillach/util/VisualizationUtil.java
    
    * Update pic-sure-util/src/main/java/edu/harvard/dbmi/avillach/util/VisualizationUtil.java
    
    * Update pic-sure-resources/pic-sure-visualization-resource/src/test/java/edu/harvard/hms/dbmi/avillach/resource/visualization/service/VisualizationUtilTests.java
    
    Co-authored-by: Luke Sikina <lucas.sikina@gmail.com>
    
    * Fix import for modified unit test
    
    * Refactor and add test cases for key size limiting function
    
    Refactors existing code by simplifying key size limiting logic in VisualizationUtil and added several new unit tests to ensure its correct behavior with different input scenarios including long keys, empty maps, null maps, and uniqueness near middle.
    
    * Update null handling in VisualizationUtil's limitKeySize
    
    Changed the handling of null input in VisualizationUtil's limitKeySize from returning a new HashMap to throwing an IllegalArgumentException. Also, updated the corresponding test to check for this exception instead of comparing with an empty map.
    
    ---------
    
    Co-authored-by: Luke Sikina <lucas.sikina@gmail.com>
    
    * Refine the condition for empty return in DataProcessingService (#184)
    
    Adjusted the condition under which an empty HashMap is returned in the DataProcessingService. Now, an empty HashMap will only be returned when both the maximum and minimum values in the data are 0, and the number of bins is also 0. This change takes into account situations where the data contains values but the min and max are both 0.
    
    * ALS-5387: Remove stack from resources table (#186)
    
    * [ALS-5422] Add persistence.xml to visualization resource (#161) (#162)
    The visualization resource is failing to start due to an error injecting persistence unit into CDI managed bean. It is unable to find a persistence unit named ''.
    
    * ALS-5387: Remove stack from resources table
    * ALS-5387: More robust implementation of feature
    
    ---------
    
    Co-authored-by: Gcolon021 <34667267+Gcolon021@users.noreply.github.com>
    Co-authored-by: GeorgeC <gcolon021@gmail.com>
    
    * [ALS-6222] Status endpoints now filters resources (#189)
    
    The database contains both open and auth hpds even if it wasn't deployed. This means we attempt to check for a resource that doesn't exist. This results in our service always showing degraded.
    
    ---------
    
    Co-authored-by: Luke Sikina <lucas.skina@gmail.com>
    Co-authored-by: Luke Sikina <lucas.sikina@gmail.com>
    Co-authored-by: ramari16 <ramari16@gmail.com>

--- pic-sure-api-war/src/main/java/edu/harvard/dbmi/avillach/service/SystemService.java
+++ pic-sure-api-war/src/main/java/edu/harvard/dbmi/avillach/service/SystemService.java
@@ -1,8 +1,10 @@
 package edu.harvard.dbmi.avillach.service;
+
 import static edu.harvard.dbmi.avillach.util.Utilities.buildHttpClientContext;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
@@ -30,108 +32,128 @@ import edu.harvard.dbmi.avillach.util.exception.ApplicationException;
 
 @Path("/system")
 public class SystemService {
-	static int max_test_frequency = 60000;
+    static int max_test_frequency = 60000;
 
-	static final String RUNNING = "RUNNING";
+    static final String RUNNING = "RUNNING";
 
-	static final String ONE_OR_MORE_COMPONENTS_DEGRADED = "ONE OR MORE COMPONENTS DEGRADED";
+    static final String ONE_OR_MORE_COMPONENTS_DEGRADED = "ONE OR MORE COMPONENTS DEGRADED";
 
-	Logger logger = LoggerFactory.getLogger(SystemService.class);
+    Logger logger = LoggerFactory.getLogger(SystemService.class);
 
-	@Inject
-	PicSureWarInit picSureWarInit;
-	
-	String lastStatus = "UNTESTED";
-	long lastStatusCheck = 0l;
+    @Inject
+    PicSureWarInit picSureWarInit;
 
-	@Inject
-	ResourceRepository resourceRepo;
-	
-	String token_introspection_url;
-	String token_introspection_token;
-	
-	@PostConstruct
-	public void init() {
-		token_introspection_url = picSureWarInit.getToken_introspection_url();
-		token_introspection_token = picSureWarInit.getToken_introspection_token();
-		if(token_introspection_url == null || token_introspection_token  == null) {
-			throw new RuntimeException(
-					"token_introspection_url and token_introspection_token not configured");
-		}
-	}
+    String lastStatus = "UNTESTED";
+    long lastStatusCheck = 0l;
 
-	@GET
-	@Path("/status")
-	@Produces("text/plain")
-	public String status() {
-		// Because there is no auth on this service we limit actually performing the checking to 1 per minute to avoid DOS scenarios.
-		long timeOfRequest = System.currentTimeMillis();
-		if(timeOfRequest-lastStatusCheck < max_test_frequency) {
-			return lastStatus;
-		}else {
-			lastStatusCheck = timeOfRequest;
-			try{
-				List<Resource> resourcesToTest = resourceRepo.list();
-				if( resourcesToTest != null &&  // This proves the MySQL database is serving queries
-						!resourcesToTest.isEmpty() && // This proves at least one resources is configured
-						testPSAMAResponds() &&  // This proves we can perform token introspection
-						testResourcesRespond(resourcesToTest) ){ // This proves all resources are at least serving info requests.
-					lastStatus = RUNNING;
-					return lastStatus;
-				}else {
-					lastStatus = ONE_OR_MORE_COMPONENTS_DEGRADED;
-				}
-			}catch(Exception e) {
-				e.printStackTrace();
-				lastStatus = ONE_OR_MORE_COMPONENTS_DEGRADED;
-			}
-			return lastStatus;
-		}
-	}
+    @Inject
+    ResourceRepository resourceRepo;
 
-	private boolean testPSAMAResponds() throws UnsupportedOperationException, IOException {
-		CloseableHttpClient client = PicSureWarInit.CLOSEABLE_HTTP_CLIENT;
-		ObjectMapper json = PicSureWarInit.objectMapper;
-		
-		HttpPost post = new HttpPost(token_introspection_url);
-		post.setEntity(new StringEntity("{}"));
-		post.setHeader("Content-Type", "application/json");
-		//Authorize into the token introspection endpoint
-		post.setHeader("Authorization", "Bearer " + token_introspection_token);
-		CloseableHttpResponse response = null;
-		try {
-			response = client.execute(post, buildHttpClientContext());
-			if (response.getStatusLine().getStatusCode() != 200){
-				logger.error("callTokenIntroEndpoint() error back from token intro host server ["
-						+ token_introspection_url + "]: " + EntityUtils.toString(response.getEntity()));
-				throw new ApplicationException("Token Introspection host server return " + response.getStatusLine().getStatusCode() +
-						". Please see the log");
-			}
-			JsonNode responseContent = json.readTree(response.getEntity().getContent());
-			if (!responseContent.get("active").asBoolean()){
-				// This is actually the expected response as we did not send a token in the token_introspection_request.
-				return true;
-			}
+    String token_introspection_url;
+    String token_introspection_token;
 
-			return true;
-		} finally {
-			try {
-				if (response != null)
-					response.close();
-			} catch (IOException ex) {
-				logger.error("callTokenIntroEndpoint() IOExcpetion when closing http response: " + ex.getMessage());
-			}
-		}
-	}
+    String defaultApplicationUUID;
 
-	private boolean testResourcesRespond(List<Resource> resourcesToTest) {
-		for(Resource resource : resourcesToTest) {
-			ResourceInfo info = new ResourceWebClient().info(resource.getResourceRSPath(), new GeneralQueryRequest());
-			if(info==null) {
-				return false;
-			}
-		}
-		return true;
-	}
+    @PostConstruct
+    public void init() {
+        token_introspection_url = picSureWarInit.getToken_introspection_url();
+        token_introspection_token = picSureWarInit.getToken_introspection_token();
+        defaultApplicationUUID = picSureWarInit.getDefaultApplicationUUID();
+        if (token_introspection_url == null || token_introspection_token == null) {
+            throw new RuntimeException("token_introspection_url and token_introspection_token not configured");
+        }
+    }
+
+    @GET
+    @Path("/status")
+    @Produces("text/plain")
+    public String status() {
+        // Because there is no auth on this service we limit actually performing the checking to 1 per minute to avoid DOS scenarios.
+        long timeOfRequest = System.currentTimeMillis();
+        if (timeOfRequest - lastStatusCheck < max_test_frequency) {
+            return lastStatus;
+        } else {
+            lastStatusCheck = timeOfRequest;
+            try {
+                List<Resource> resourcesToTest = resourceRepo.list();
+                if (resourcesToTest == null || resourcesToTest.isEmpty()) {
+                    lastStatus = ONE_OR_MORE_COMPONENTS_DEGRADED;
+                    return lastStatus;
+                }
+
+                // convert the default application uuid to an uuid object
+                UUID defaultApplicationUUID = UUID.fromString(this.defaultApplicationUUID);
+
+                // We need to remove open or auth HPDS from the resource list depending on the environment deployed.
+                // This because both are included in the database, but only one is actually deployed.
+                // if the name contains hpds and is not the default application uuid, remove it.
+                resourcesToTest.removeIf(
+                    resource -> resource.getName().toLowerCase().contains("hpds") && !resource.getUuid().equals(defaultApplicationUUID)
+                );
+
+                // This proves the MySQL database is serving queries
+                // This proves at least one resources is configured
+                // This proves we can perform token introspection
+                if (testPSAMAResponds() && testResourcesRespond(resourcesToTest)) { // This proves all resources are at least serving info
+                                                                                    // requests.
+                    lastStatus = RUNNING;
+                    return lastStatus;
+                } else {
+                    lastStatus = ONE_OR_MORE_COMPONENTS_DEGRADED;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                lastStatus = ONE_OR_MORE_COMPONENTS_DEGRADED;
+            }
+            return lastStatus;
+        }
+    }
+
+    private boolean testPSAMAResponds() throws UnsupportedOperationException, IOException {
+        CloseableHttpClient client = PicSureWarInit.CLOSEABLE_HTTP_CLIENT;
+        ObjectMapper json = PicSureWarInit.objectMapper;
+
+        HttpPost post = new HttpPost(token_introspection_url);
+        post.setEntity(new StringEntity("{}"));
+        post.setHeader("Content-Type", "application/json");
+        // Authorize into the token introspection endpoint
+        post.setHeader("Authorization", "Bearer " + token_introspection_token);
+        CloseableHttpResponse response = null;
+        try {
+            response = client.execute(post, buildHttpClientContext());
+            if (response.getStatusLine().getStatusCode() != 200) {
+                logger.error(
+                    "callTokenIntroEndpoint() error back from token intro host server [" + token_introspection_url + "]: "
+                        + EntityUtils.toString(response.getEntity())
+                );
+                throw new ApplicationException(
+                    "Token Introspection host server return " + response.getStatusLine().getStatusCode() + ". Please see the log"
+                );
+            }
+            JsonNode responseContent = json.readTree(response.getEntity().getContent());
+            if (!responseContent.get("active").asBoolean()) {
+                // This is actually the expected response as we did not send a token in the token_introspection_request.
+                return true;
+            }
+
+            return true;
+        } finally {
+            try {
+                if (response != null) response.close();
+            } catch (IOException ex) {
+                logger.error("callTokenIntroEndpoint() IOExcpetion when closing http response: " + ex.getMessage());
+            }
+        }
+    }
+
+    private boolean testResourcesRespond(List<Resource> resourcesToTest) {
+        for (Resource resource : resourcesToTest) {
+            ResourceInfo info = new ResourceWebClient().info(resource.getResourceRSPath(), new GeneralQueryRequest());
+            if (info == null) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
