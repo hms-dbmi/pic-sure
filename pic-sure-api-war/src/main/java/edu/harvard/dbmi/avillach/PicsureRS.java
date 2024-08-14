99069d8c15c60bf16515062ce6f401edca0c35bc
Ryan Amari <ramari16@gmail.com>
2024-08-14 15:43:15 -0400

    Attempt to fix encoding issue

--- pic-sure-api-war/src/main/java/edu/harvard/dbmi/avillach/PicsureRS.java
+++ pic-sure-api-war/src/main/java/edu/harvard/dbmi/avillach/PicsureRS.java
@@ -155,6 +155,7 @@ public class PicsureRS {
             responseCode = "200", description = "Query result", content = @Content(schema = @Schema(implementation = Response.class))
         )}
     )
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
     public Response queryResult(
         @Parameter(
             description = "The UUID of the query to fetch the status of. The UUID is "
