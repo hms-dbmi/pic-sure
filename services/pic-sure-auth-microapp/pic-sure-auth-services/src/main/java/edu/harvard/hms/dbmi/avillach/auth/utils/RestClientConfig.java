4fe4a6df5ad2f61668dd21b4093128c354224edc
Luke Sikina <lucas.skina@gmail.com>
2024-08-20 14:15:50 -0400

    [ALS-6880] HttpClient with proxy

---
+++ services/pic-sure-auth-microapp/pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/utils/RestClientConfig.java
@@ -0,0 +1,51 @@
+package edu.harvard.hms.dbmi.avillach.auth.utils;
+
+
+import org.apache.hc.client5.http.classic.HttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.util.StringUtils;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestClientConfig {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RestClientConfig.class);
+
+    @Value("${http.proxyUser:}")
+    private String proxyUser;
+
+    @Value("${http.proxyPassword:}")
+    private String proxyPassword;
+
+    @Bean
+    public HttpClient getHttpClient() {
+        if (!StringUtils.hasLength(proxyUser)) {
+            return HttpClients.createDefault();
+        }
+        LOG.info("Found proxy user {}, will configure proxy", proxyUser);
+        PoolingHttpClientConnectionManager manager = new PoolingHttpClientConnectionManager();
+        manager.setMaxTotal(100);
+        return HttpClients
+            .custom()
+            .setConnectionManager(new PoolingHttpClientConnectionManager())
+            .useSystemProperties()
+            .build();
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate(@Autowired HttpClient client) {
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        clientHttpRequestFactory.setHttpClient(client);
+        return new RestTemplate(factory);
+    }
+}
--- services/pic-sure-auth-microapp/pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/utils/RestClientUtil.java
+++ services/pic-sure-auth-microapp/pic-sure-auth-services/src/main/java/edu/harvard/hms/dbmi/avillach/auth/utils/RestClientUtil.java
@@ -2,6 +2,7 @@ package edu.harvard.hms.dbmi.avillach.auth.utils;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
@@ -15,7 +16,12 @@ import org.springframework.web.client.RestTemplate;
 public class RestClientUtil {
 
     private static final Logger logger = LoggerFactory.getLogger(RestClientUtil.class);
-    private final RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public RestClientUtil(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     public ResponseEntity<String> retrieveGetResponse(String uri, HttpHeaders headers) {
         try {
