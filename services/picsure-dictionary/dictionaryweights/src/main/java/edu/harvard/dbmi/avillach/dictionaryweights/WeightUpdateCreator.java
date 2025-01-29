5cbc72e758d02b1c472d870c6d4184a95fff0b9a
Luke Sikina <lucas.skina@gmail.com>
2025-01-29 12:11:30 -0500

    [ALS-8191] Switch from study to dataset for weights job

--- services/picsure-dictionary/dictionaryweights/src/main/java/edu/harvard/dbmi/avillach/dictionaryweights/WeightUpdateCreator.java
+++ services/picsure-dictionary/dictionaryweights/src/main/java/edu/harvard/dbmi/avillach/dictionaryweights/WeightUpdateCreator.java
@@ -42,9 +42,23 @@ public class WeightUpdateCreator {
                         GROUP BY
                             concept_node.concept_node_id
                     ) AS concept_node_meta_str ON concept_node_meta_str.id = concept_node.concept_node_id
-                    LEFT JOIN dataset ON concept_node.dataset_id = dataset.dataset_id
+                    LEFT JOIN dataset AS study ON concept_node.dataset_id = study.dataset_id
                     LEFT JOIN concept_node AS parent ON concept_node.parent_id = parent.concept_node_id
                     LEFT JOIN concept_node AS grandparent ON parent.parent_id = grandparent.concept_node_id
+                    LEFT JOIN (
+                        SELECT
+                            dataset_node.name AS NAME,
+                            dataset_node.display AS FULL_NAME,
+                            dataset_node.concept_path AS CONCEPT_PATH,
+                            description_concept_node_meta.VALUE AS DESCRIPTION
+                        FROM
+                            concept_node AS dataset_node
+                            JOIN concept_node_meta AS description_concept_node_meta ON description_concept_node_meta.KEY = 'description' AND description_concept_node_meta.concept_node_id = dataset_node.concept_node_id
+                    ----------------------------------------------------------------------------------------------------
+                    -- This join is saying "find me the concept node that shares the first two nodes of this concept
+                    -- and then stops". So for /a/b/c/d/e/f/, it finds concept /a/b/
+                    ----------------------------------------------------------------------------------------------------
+                    ) AS dataset ON dataset.concept_path = REGEXP_REPLACE(concept_node.concept_path, '(^\\\\[^\\\\]*\\\\[^\\\\]*\\\\)(.*$)', '\\1')
             ) AS data_table
             WHERE concept_node.concept_node_id = data_table.search_key;
             """.formatted(searchableFields);
