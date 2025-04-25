db273e0e4e23aea1f2a6d73031248813a5923f34
ramari16 <ramari16@gmail.com>
2025-04-25 14:04:04 -0400

    ALS-8899: Update Job to build bucket index files, etc (#147)

--- services/pic-sure-hpds/etl/src/main/java/edu/harvard/hms/dbmi/avillach/hpds/etl/genotype/GenomicDatasetFinalizer.java
+++ services/pic-sure-hpds/etl/src/main/java/edu/harvard/hms/dbmi/avillach/hpds/etl/genotype/GenomicDatasetFinalizer.java
@@ -2,20 +2,74 @@ package edu.harvard.hms.dbmi.avillach.hpds.etl.genotype;
 
 import edu.harvard.hms.dbmi.avillach.hpds.data.genotype.BucketIndexBySample;
 import edu.harvard.hms.dbmi.avillach.hpds.data.genotype.VariantStore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+import java.util.zip.GZIPOutputStream;
 
 public class GenomicDatasetFinalizer {
-    private static String genomicDirectory;
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        if (args.length != 2) {
-            throw new IllegalArgumentException("2 arguments must be provided: source directory 1, source directory 2, output directory");
+    private static final Logger log = LoggerFactory.getLogger(GenomicDatasetFinalizer.class);
+    private static final String genomicDirectory = "/opt/local/hpds/all/";
+    private final File[] chromosomeDirectories;
+    private final Semaphore sem;
+
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        new GenomicDatasetFinalizer(genomicDirectory, 10).processDirectory();
+    }
+
+    public GenomicDatasetFinalizer(String genomicDirectory, int maxThreads) {
+        chromosomeDirectories = new File(genomicDirectory).listFiles(File::isDirectory);
+        if (chromosomeDirectories == null) {
+            throw new IllegalArgumentException("Path " + genomicDirectory + " does not contain any directories");
         }
-        genomicDirectory = args[0];
-        String outputDirectory = args[1];
+        if (chromosomeDirectories.length > 50) {
+            throw new IllegalArgumentException("Number of chromosome partitions exceeds maximum of 50 (" + chromosomeDirectories.length + ")");
+        }
+        sem = new Semaphore(maxThreads);
+    }
 
-        VariantStore variantStore = VariantStore.readInstance(genomicDirectory);
-        BucketIndexBySample bucketIndexBySample = new BucketIndexBySample(variantStore, genomicDirectory);
+
+    public void processDirectory() {
+        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            for (File chromosomeDirectory : chromosomeDirectories) {
+                sem.acquire();
+                executor.submit(() -> {
+                    try {
+                        String bucketIndexDirectory = chromosomeDirectory.getPath() + "/";
+                        VariantStore variantStore = VariantStore.readInstance(bucketIndexDirectory);
+                        BucketIndexBySample bucketIndexBySample = new BucketIndexBySample(variantStore, bucketIndexDirectory);
+                        String bucketIndexFilename = bucketIndexDirectory + "BucketIndexBySample.javabin";
+                        log.info("creating new " + bucketIndexFilename);
+                        try (
+                                FileOutputStream fos = new FileOutputStream(bucketIndexFilename);
+                                GZIPOutputStream gzos = new GZIPOutputStream(fos);
+                                ObjectOutputStream oos = new ObjectOutputStream(gzos);
+                        ){
+                            oos.writeObject(bucketIndexBySample);
+                            oos.flush();
+                        }
+                    } catch (IOException | ClassNotFoundException e) {
+                        executor.shutdown();
+                        throw new RuntimeException(e);
+                    }
+                    sem.release();
+                });
+            }
+            executor.shutdown();
+            while (!executor.awaitTermination(1, TimeUnit.MINUTES)) {
+                log.info("Awaiting threadpool termination");
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- services/pic-sure-hpds/service/src/test/java/edu/harvard/hms/dbmi/avillach/hpds/test/util/BuildIntegrationTestEnvironment.java
+++ services/pic-sure-hpds/service/src/test/java/edu/harvard/hms/dbmi/avillach/hpds/test/util/BuildIntegrationTestEnvironment.java
@@ -1,43 +1,24 @@
 package edu.harvard.hms.dbmi.avillach.hpds.test.util;
 
-import edu.harvard.hms.dbmi.avillach.hpds.data.genotype.BucketIndexBySample;
-import edu.harvard.hms.dbmi.avillach.hpds.data.genotype.VariantStore;
-import edu.harvard.hms.dbmi.avillach.hpds.etl.genotype.NewVCFLoader;
-import edu.harvard.hms.dbmi.avillach.hpds.etl.genotype.SplitChromosomeVariantMetadataLoader;
-import edu.harvard.hms.dbmi.avillach.hpds.etl.genotype.SplitChromosomeVcfLoader;
-import edu.harvard.hms.dbmi.avillach.hpds.etl.genotype.VariantMetadataLoader;
+import edu.harvard.hms.dbmi.avillach.hpds.etl.genotype.*;
 import edu.harvard.hms.dbmi.avillach.hpds.etl.phenotype.csv.CSVLoader;
 
 import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.util.List;
 
 public enum BuildIntegrationTestEnvironment {
     INSTANCE;
 
     private static final String PHENOTYPIC_DATA_DIRECTORY = "target/test-classes/phenotypic/";
-    private final String VCF_INDEX_FILE = "./src/test/resources/test_vcfIndex.tsv";
-    private final String STORAGE_DIR = "./target/all/";
-    private final String MERGED_DIR = "./target/merged/";
-
-    public String binFile = "target/VariantMetadata.javabin";
+    private static final String VCF_INDEX_FILE = "./src/test/resources/test_vcfIndex.tsv";
+    private static final String STORAGE_DIR = "./target/all/";
+    private static final String MERGED_DIR = "./target/merged/";
 
     BuildIntegrationTestEnvironment() {
         try {
             SplitChromosomeVcfLoader.main(new String[] {VCF_INDEX_FILE, STORAGE_DIR, MERGED_DIR});
             CSVLoader.main(new String[] {PHENOTYPIC_DATA_DIRECTORY});
             SplitChromosomeVariantMetadataLoader.main(new String[] {"./src/test/resources/test_vcfIndex.tsv", STORAGE_DIR});
-            List.of("chr4", "chr20", "chr21").forEach(dir -> {
-                VariantStore variantStore = null;
-                try {
-                    variantStore = VariantStore.readInstance(STORAGE_DIR + dir + "/");
-                    BucketIndexBySample bucketIndexBySample = new BucketIndexBySample(variantStore, STORAGE_DIR + dir + "/");
-                } catch (IOException e) {
-                    throw new UncheckedIOException(e);
-                } catch (ClassNotFoundException e) {
-                    throw new RuntimeException(e);
-                }
-            });
+            new GenomicDatasetFinalizer(STORAGE_DIR, 10).processDirectory();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
