b519865a0853e520fc8026af9968446cb850bba7
Luke Sikina <lucas.skina@gmail.com>
2025-09-15 14:20:49 -0400

    [ALS-9230] Findings from UTHSC prod: schema was altered
    - I messed up and changed the schema directly instead of writing a
    migrator
    - I then also wrote a migrator, which breaks because the schema was
    modified
    - Fix: revert schema change. mb

--- services/picsure-dictionary/db/schema.sql
+++ services/picsure-dictionary/db/schema.sql
@@ -118,5 +118,3 @@ CREATE TABLE IF NOT EXISTS dict.dataset_harmonization
         REFERENCES dict.dataset (dataset_id)
 );
 
-ALTER TABLE dict.concept_node ADD COLUMN concept_path_md5 text GENERATED ALWAYS AS (md5(concept_path::text)) STORED;
-CREATE UNIQUE INDEX idx_concept_node_md5 ON dict.concept_node(concept_path_md5);
