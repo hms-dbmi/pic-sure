51b9fc8cc521437b62c9b7905f739935eef439ac
ramari16 <ramari16@gmail.com>
2025-12-02 15:20:31 -0500

    ALS-10388: Move cache configuration to properties files (#89)

--- services/picsure-dictionary/src/main/java/edu/harvard/dbmi/avillach/dictionary/util/CacheConfig.java
+++ services/picsure-dictionary/src/main/java/edu/harvard/dbmi/avillach/dictionary/util/CacheConfig.java
@@ -1,6 +1,7 @@
 package edu.harvard.dbmi.avillach.dictionary.util;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
@@ -10,9 +11,16 @@ import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class CacheConfig {
+
+    @Value("${cache.duration-minutes:60}")
+    private int cacheDurationMinutes;
+
+    @Value("${cache.maximum-size:5000}")
+    private int cacheMaximumSize;
+
     @Bean
     public Caffeine caffeineConfig() {
-        return Caffeine.newBuilder().expireAfterAccess(15, TimeUnit.MINUTES).maximumSize(5000);
+        return Caffeine.newBuilder().expireAfterAccess(cacheDurationMinutes, TimeUnit.MINUTES).maximumSize(cacheMaximumSize);
     }
 
     @Bean
